5d82ccc2310c986829ca10003a3b4903844ede5c
Thierry Lacour <thi@praqma.net>
2017-10-13 14:57:00 +0200

    WIP - trying out some stuff, wrote a comment in OSCA.java

--- src/main/java/org/jenkinsci/plugins/systemconfigdsl/OnStartupConfigurationApplicator.java
+++ src/main/java/org/jenkinsci/plugins/systemconfigdsl/OnStartupConfigurationApplicator.java
@@ -1,12 +1,15 @@
 package org.jenkinsci.plugins.systemconfigdsl;
 
+import hudson.ExtensionList;
+import hudson.PluginWrapper;
 import hudson.init.Initializer;
+import jenkins.model.GlobalConfiguration;
 import jenkins.model.Jenkins;
-import org.jenkinsci.plugins.systemconfigdsl.api.Configurator;
-import org.jenkinsci.plugins.systemconfigdsl.error.ConfigurationNotFoundException;
 
 import java.io.IOException;
-import java.util.Map;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
 import static hudson.init.InitMilestone.EXTENSIONS_AUGMENTED;
@@ -16,29 +19,59 @@ import static hudson.init.InitMilestone.JOB_LOADED;
 public class OnStartupConfigurationApplicator {
     private static final Logger LOGGER = Logger.getLogger(OnStartupConfigurationApplicator.class.getName());
 
-    @Initializer(after=EXTENSIONS_AUGMENTED,before=JOB_LOADED)
-    public static void init(Jenkins j) throws IOException {
-        new OnStartupConfigurationApplicator().apply();
-    }
+    @Initializer(after = EXTENSIONS_AUGMENTED, before = JOB_LOADED)
+    public static void init(Jenkins jenkins) throws IOException {
 
-    public void apply() throws IOException {
+        /*
+            The goal:
+                Be able to structure YAML file to match Jenkins UI
+            Practical implementation:
+                Can we use a plugin's DataboundSetters, public properties or jelly to configure it?
 
-        final ConfigurationLoader configurationLoader;
-        try {
-            configurationLoader = new ConfigurationLoader();
-        } catch (ConfigurationNotFoundException e) {
-            LOGGER.warning("Can't load configuration due to: " + e.getMessage() + ". Will not continue with configuration as code");
-            return;
+                Brainstorm #1
+                1. call DataboundSetters on plugins directly
+
+                Brainstorm #2
+                1. Parse jelly
+                2. Build config using jelly/YAML file.
+                3. Configure plugin with  built config
+         */
+
+        LOGGER.info("--------------------------------------------------------");
+        LOGGER.info("--------------------------------------------------------");
+        LOGGER.info("--------------------------------------------------------");
+
+        LOGGER.info("OnStartupConfigurationApplicator.init");
+
+        ExtensionList<GlobalConfiguration> globalConfigurations = jenkins.getExtensionList(GlobalConfiguration.class);
+        for (GlobalConfiguration globalConfig : globalConfigurations) {
+            LOGGER.info("globalConfig: " + globalConfig.getDisplayName());
+
+            Constructor[] constructors = globalConfig.getClass().getDeclaredConstructors();
+            for (Constructor constructor : constructors) {
+                LOGGER.info("CONSTRUCTOR: " + constructor.getName());
+                Annotation[] annotations = constructor .getDeclaredAnnotations();
+                for (Annotation annotation : annotations) {
+                    LOGGER.info(" - ANNOTATION - " + annotation.annotationType().getName());
+                }
+            }
+
+            Method[] methods = globalConfig.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                LOGGER.info("METHOD: " + method.getName());
+                Annotation[] annotations = method.getDeclaredAnnotations();
+                for (Annotation annotation : annotations) {
+                    LOGGER.info(" - ANNOTATION - " + annotation.annotationType().getName());
+                }
+            }
         }
 
-        LOGGER.info("Applying configuration from " + configurationLoader.getConfigurationDir().getAbsolutePath());
+        LOGGER.info("--------------------------------------------------------");
+        LOGGER.info("--------------------------------------------------------");
+        LOGGER.info("--------------------------------------------------------");
 
-        final ServiceImplementationsLoader serviceLoader = new ServiceImplementationsLoader();
-        final Map<String,Configurator> configurators = serviceLoader.getConfigurators();
-
-        final Map<String, String> configurations = configurationLoader.loadConfiguration();
-
-        ConfigurationApplicator applicator = new ConfigurationApplicator();
-        applicator.applyConfiguration(configurations, configurators, false);
+        for (PluginWrapper plugin : jenkins.getPluginManager().getPlugins()) {
+            LOGGER.info("plugin: " + plugin.getDisplayName());
+        }
     }
 }
