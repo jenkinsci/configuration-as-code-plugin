9779e56befc5ca18eccb49b42b32d7301b42d561
Praqma Service User <man@praqma.net>
2018-01-26 15:52:30 +0100

    [WiP] Move stuff from reveal in VaultSecretSource to constructor as suggested by @ndeloof. See #19

--- src/main/java/org/jenkinsci/plugins/casc/VaultSecretSource.java
+++ src/main/java/org/jenkinsci/plugins/casc/VaultSecretSource.java
@@ -5,6 +5,8 @@ import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
 import hudson.Extension;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,9 +20,9 @@ import java.util.logging.Logger;
 public class VaultSecretSource extends SecretSource {
 
     private final static Logger LOGGER = Logger.getLogger(VaultSecretSource.class.getName());
+    private Map<String, String> secrets = new HashMap<>();
 
-    @Override
-    public Optional<String> reveal(String vaultKey) {
+    public VaultSecretSource() {
         String vaultPw = System.getenv("CASC_VAULT_PW");
         String vaultUsr = System.getenv("CASC_VAULT_USER");
         String vaultPth = System.getenv("CASC_VAULT_PATH");
@@ -33,17 +35,27 @@ public class VaultSecretSource extends SecretSource {
                 //Obtain a login token
                 final String token = vault.auth().loginByUserPass(vaultUsr, vaultPw).getAuthClientToken();
                 config.token(token).build();
-                //Fetch
-                String v = vault.logical().read(vaultPth).getData().get(vaultKey);
-                if (v != null)
-                    returnValue = Optional.of(v);
-                return returnValue;
+                secrets = vault.logical().read(vaultPth).getData();
             } catch (VaultException ve) {
                 LOGGER.log(Level.WARNING, "Unable to fetch password from vault", ve);
-                return returnValue;
             }
+        }
+    }
+
+    @Override
+    public Optional<String> reveal(String vaultKey) {
+        Optional<String> returnValue = Optional.empty();
+        if(secrets.get(vaultKey) != null)  {
+            returnValue = Optional.of(secrets.get(vaultKey));
         }
         return returnValue;
     }
 
+    public Map<String, String> getSecrets() {
+        return secrets;
+    }
+
+    public void setSecrets(Map<String, String> secrets) {
+        this.secrets = secrets;
+    }
 }
