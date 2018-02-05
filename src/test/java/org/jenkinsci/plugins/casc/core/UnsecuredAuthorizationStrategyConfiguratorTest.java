469828028620b43c79137577c34d8776f4b357f2
Kohsuke Kawaguchi <kk@kohsuke.org>
2018-02-05 14:02:00 +0100

    Added a test case

---
+++ src/test/java/org/jenkinsci/plugins/casc/core/UnsecuredAuthorizationStrategyConfiguratorTest.java
@@ -0,0 +1,23 @@
+package org.jenkinsci.plugins.casc.core;
+
+import hudson.security.AuthorizationStrategy;
+import org.jenkinsci.plugins.casc.ConfigurationAsCode;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class UnsecuredAuthorizationStrategyConfiguratorTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void configure_local_security_and_admin_user() throws Exception {
+        ConfigurationAsCode.configure(getClass().getResourceAsStream("UnsecuredAuthorizationStrategyConfiguratorTest.yml"));
+        assertSame(AuthorizationStrategy.UNSECURED, j.jenkins.getAuthorizationStrategy());
+    }
+}
