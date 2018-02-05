bae8aaf78fd3afe1edc2ef23508af6f0044a3a5d
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-02-05 16:20:30 +0100

    Fix #48 - Add support of Role Strategy plugin

---
+++ src/main/java/org/jenkinsci/plugins/casc/integrations/RoleDefinition.java
@@ -0,0 +1,38 @@
+package org.jenkinsci.plugins.casc.integrations;
+
+import com.michelin.cio.hudson.plugins.rolestrategy.Role;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Role definition.
+ * Used for custom formatting
+ * @author Oleg Nenashev
+ * @since TODO
+ */
+@Restricted(NoExternalUse.class)
+public class RoleDefinition {
+
+    private final Role role;
+    private final Set<String> assignments;
+
+    @DataBoundConstructor
+    public RoleDefinition(Role role, Collection<String> assignments) {
+        this.role = role;
+        this.assignments = assignments != null ? new HashSet<>(assignments) : Collections.emptySet();
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Set<String> getAssignments() {
+        return Collections.unmodifiableSet(assignments);
+    }
+}
---
+++ src/test/java/org/jenkinsci/plugins/casc/integrations/RoleStrategyTest.java
@@ -0,0 +1,66 @@
+package org.jenkinsci.plugins.casc.integrations;
+
+import com.michelin.cio.hudson.plugins.rolestrategy.Role;
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy;
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleMap;
+import hudson.security.AuthorizationStrategy;
+import jenkins.model.Jenkins;
+import static org.hamcrest.CoreMatchers.*;
+import org.jenkinsci.plugins.casc.ConfigurationAsCode;
+import org.jenkinsci.plugins.casc.Configurator;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Oleg Nenashev
+ * @since TODO
+ */
+public class RoleStrategyTest {
+
+    @ClassRule
+    public static JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void shouldReturnCustomConfigurator() {
+        Configurator c = Configurator.lookup(RoleBasedAuthorizationStrategy.class);
+        assertNotNull("Failed to find configurator for RoleBasedAuthorizationStrategy", c);
+        assertEquals("Retrieved wrong configurator", RoleBasedAuthorizationStrategyConfigurator.class, c.getClass());
+    }
+
+    @Test
+    @Issue("Issue #59")
+    public void shouldReturnCustomConfiguratorForBaseType() {
+        Configurator c = Configurator.lookupForBaseType(AuthorizationStrategy.class, "roleStrategy");
+        assertNotNull("Failed to find configurator for RoleBasedAuthorizationStrategy", c);
+        assertEquals("Retrieved wrong configurator", RoleBasedAuthorizationStrategyConfigurator.class, c.getClass());
+
+        Configurator.lookup(RoleBasedAuthorizationStrategy.class);
+    }
+
+    @Test
+    @Issue("Issue #48")
+    public void shouldReadRolesCorrectly() throws Exception {
+        ConfigurationAsCode.configure(getClass().getResourceAsStream("role-strategy/RoleStrategy1.yml"));
+
+        final Jenkins jenkins = Jenkins.getInstance();
+        AuthorizationStrategy s = jenkins.getAuthorizationStrategy();
+        assertThat("Authorization Strategy has been read incorrectly",
+                s, instanceOf(RoleBasedAuthorizationStrategy.class));
+        RoleBasedAuthorizationStrategy rbas = (RoleBasedAuthorizationStrategy)s;
+
+        Map<Role, Set<String>> globalRoles = rbas.getGrantedRoles(RoleBasedAuthorizationStrategy.GLOBAL);
+        assertThat(globalRoles.size(), equalTo(2));
+
+        //TODO: add more checks to the test
+    }
+}
