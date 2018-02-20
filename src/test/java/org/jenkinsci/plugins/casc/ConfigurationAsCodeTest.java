384710e30050c6a1b534cd10de59bcac26dcadff
Kirill Merkushev <lanwen@yandex.ru>
2018-02-20 14:08:50 +0300

    safe way to scan for configs
    This commit adds resource control for all files, as previously the
    stream close was neglected which may lead to resource leaks.
    
    This commit uses stream api to search configs and run reconfiguration.
    The stream api here allows to reduce code complexity as we get
    flat flow of paths, that converted to entries in config and then
    processed by configurators.
    
    In case of files search stream api gives us one line to get the flat
    stream of paths - it allows us to pay more attention to error handling
    
    Passing InputStreams by reference between method without error handling
    usually looks like antipattern, so all the file logic is concentrated
    inside of dedicated methods.

--- src/test/java/org/jenkinsci/plugins/casc/ConfigurationAsCodeTest.java
+++ src/test/java/org/jenkinsci/plugins/casc/ConfigurationAsCodeTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 
 public class ConfigurationAsCodeTest {
 
@@ -16,9 +18,21 @@ public class ConfigurationAsCodeTest {
     @Test
     public void init_test_from_accepted_sources() throws Exception {
         ConfigurationAsCode casc = new ConfigurationAsCode();
-        File tmpConfigFile = tempFolder.newFile("jenkins_tmp.yaml");
+
+        File exactFile = tempFolder.newFile("jenkins_tmp.yaml");
         tempFolder.newFile("jenkins_tmp2.yaml");
-        assertEquals(1, casc.getConfigurationInputs(tmpConfigFile.getAbsolutePath()).size());
-        assertEquals(2, casc.getConfigurationInputs(tempFolder.getRoot().getAbsolutePath()).size());
+        tempFolder.newFile("jenkins_tmp3.YAML");
+        tempFolder.newFile("jenkins_tmp4.YML");
+        tempFolder.newFile("jenkins_tmp5.yml");
+        tempFolder.newFolder("jenkins_folder.yml");
+
+        assertThat(casc.configs(exactFile.getAbsolutePath()), hasSize(1));
+        assertThat(casc.configs(tempFolder.getRoot().getAbsolutePath()), hasSize(5));
+    }
+
+    @Test
+    public void shouldReturnEmptyMapOnNotFoundConfig() {
+        ConfigurationAsCode casc = new ConfigurationAsCode();
+        assertThat(casc.configs("some"), hasSize(0));
     }
 }
