9b9804bb58976647b98fc4ed69a581f26499993a
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-02-26 10:03:34 +0100

    Role Strategy Fix + Cleanup + PermissionAssert (#131)
    * Move PermissionFinder to an utility package and restrict it
    
    * Fix #99 - Now RoleStrategy uses standartized PermissionFinder
    
    * Add logic for Permission assert
    
    * #99  - Add tests for Role Strategy with proper coverage

--- src/main/java/org/jenkinsci/plugins/casc/util/PermissionFinder.java
+++ src/main/java/org/jenkinsci/plugins/casc/util/PermissionFinder.java
@@ -1,8 +1,9 @@
-package org.jenkinsci.plugins.casc.integrations.globalmatrixauth;
+package org.jenkinsci.plugins.casc.util;
 
 import hudson.security.Permission;
 import hudson.security.PermissionGroup;
-import jenkins.model.Jenkins;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 import javax.annotation.CheckForNull;
 import java.util.List;
@@ -11,8 +12,10 @@ import java.util.regex.Pattern;
 
 
 /**
+ * Implements lookup for {@link Permission}s.
  * Created by mads on 2/9/18.
  */
+@Restricted(NoExternalUse.class)
 public class PermissionFinder {
 
     /** For Matrix Auth - Title/Permission **/
@@ -20,12 +23,22 @@ public class PermissionFinder {
 
     /**
      * Attempt to match a given permission to what is defined in the UI.
-     * TODO: Refactor this away when proper permission API is in place for C-as-C
      * @param id String of the form "Title/Permission" (Look in the UI) for a particular permission
      * @return a matched permission
      */
     @CheckForNull
     public static Permission findPermission(String id) {
+        final String resolvedId = findPermissionId(id);
+        return resolvedId != null ? Permission.fromId(resolvedId) : null;
+    }
+
+    /**
+     * Attempt to match a given permission to what is defined in the UI.
+     * @param id String of the form "Title/Permission" (Look in the UI) for a particular permission
+     * @return a matched permission ID
+     */
+    @CheckForNull
+    public static String findPermissionId(String id) {
         List<PermissionGroup> pgs = PermissionGroup.getAll();
         Matcher m = PERMISSION_PATTERN.matcher(id);
         if(m.matches()) {
@@ -35,11 +48,13 @@ public class PermissionFinder {
                 if(pg.owner.equals(Permission.class)) {
                     continue;
                 }
+
+                //TODO: this logic uses a localizable field for resolution. It may blow up at any moment
                 //How do we do this properly, we want to mimic the UI as best as possible. So the logic conclusion is
                 //That when you want admin to be Overall/Administer you put that in. Overall being the group title...
-                //Name being the Permssion you want to set in the matrix.
+                //Name being the Permission you want to set in the matrix.
                 if(pg.title.toString().equals(owner)) {
-                    return Permission.fromId(pg.owner.getName()+"."+name);
+                    return pg.owner.getName() + "." + name;
                 }
             }
         }
--- src/test/java/org/jenkinsci/plugins/casc/integrations/rolebasedauth/RoleStrategyTest.java
+++ src/test/java/org/jenkinsci/plugins/casc/integrations/rolebasedauth/RoleStrategyTest.java
@@ -1,12 +1,18 @@
 package org.jenkinsci.plugins.casc.integrations.rolebasedauth;
 
+import com.cloudbees.hudson.plugins.folder.Folder;
 import com.michelin.cio.hudson.plugins.rolestrategy.Role;
 import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy;
+import hudson.model.Computer;
+import hudson.model.FreeStyleProject;
+import hudson.model.Item;
+import hudson.model.User;
 import hudson.security.AuthorizationStrategy;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.casc.Configurator;
 import org.jenkinsci.plugins.casc.misc.ConfiguredWithCode;
 import org.jenkinsci.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import static org.jenkinsci.plugins.casc.misc.PermissionAssert.*;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
@@ -50,9 +56,18 @@ public class RoleStrategyTest {
     @Issue("Issue #48")
     @ConfiguredWithCode("RoleStrategy1.yml")
     public void shouldReadRolesCorrectly() throws Exception {
-        final Jenkins jenkins = Jenkins.getInstance();
+        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
+        User admin = User.get("admin");
+        User user1 = User.get("user1");
+        User user2 = User.get("user2");
+        Computer agent1 = j.jenkins.getComputer("agent1");
+        Computer agent2 = j.jenkins.getComputer("agent2");
+        Folder folderA = j.jenkins.createProject(Folder.class, "A");
+        FreeStyleProject jobA1 = folderA.createProject(FreeStyleProject.class, "1");
+        Folder folderB = j.jenkins.createProject(Folder.class, "B");
+        FreeStyleProject jobB2 = folderB.createProject(FreeStyleProject.class, "2");
 
-        AuthorizationStrategy s = jenkins.getAuthorizationStrategy();
+        AuthorizationStrategy s = j.jenkins.getAuthorizationStrategy();
         assertThat("Authorization Strategy has been read incorrectly",
                 s, instanceOf(RoleBasedAuthorizationStrategy.class));
         RoleBasedAuthorizationStrategy rbas = (RoleBasedAuthorizationStrategy) s;
@@ -60,6 +75,33 @@ public class RoleStrategyTest {
         Map<Role, Set<String>> globalRoles = rbas.getGrantedRoles(RoleBasedAuthorizationStrategy.GLOBAL);
         assertThat(globalRoles.size(), equalTo(2));
 
-        //TODO: add more checks to the test
+        // Admin has configuration access
+        assertHasPermission(admin, j.jenkins, Jenkins.ADMINISTER, Jenkins.READ);
+        assertHasPermission(user1, j.jenkins, Jenkins.READ);
+        assertHasNoPermission(user1, j.jenkins, Jenkins.ADMINISTER, Jenkins.RUN_SCRIPTS);
+
+        // Folder A is restricted to admin
+        assertHasPermission(admin, folderA, Item.CONFIGURE);
+        assertHasPermission(user1, folderA, Item.READ, Item.DISCOVER);
+        assertHasNoPermission(user1, folderA, Item.CONFIGURE, Item.DELETE, Item.BUILD);
+
+        // But they have access to jobs in Folder A
+        assertHasPermission(admin, folderA, Item.CONFIGURE, Item.CANCEL);
+        assertHasPermission(user1, jobA1, Item.READ, Item.DISCOVER, Item.CONFIGURE, Item.BUILD, Item.DELETE);
+        assertHasPermission(user2, jobA1, Item.READ, Item.DISCOVER, Item.CONFIGURE, Item.BUILD, Item.DELETE);
+        assertHasNoPermission(user1, folderA, Item.CANCEL);
+
+        // FolderB is editable by user2, but he cannot delete it
+        assertHasPermission(user2, folderB, Item.READ, Item.DISCOVER, Item.CONFIGURE, Item.BUILD);
+        assertHasNoPermission(user2, folderB, Item.DELETE);
+        assertHasNoPermission(user1, folderB, Item.CONFIGURE, Item.BUILD, Item.DELETE);
+
+        // Only user1 can run on agent1, but he still cannot configure it
+        assertHasPermission(admin, agent1, Computer.CONFIGURE, Computer.DELETE, Computer.BUILD);
+        assertHasPermission(user1, agent1, Computer.BUILD);
+        assertHasNoPermission(user1, agent1, Computer.CONFIGURE, Computer.DISCONNECT);
+
+        // Same user still cannot build on agent2
+        assertHasNoPermission(user1, agent2, Computer.BUILD);
     }
 }
