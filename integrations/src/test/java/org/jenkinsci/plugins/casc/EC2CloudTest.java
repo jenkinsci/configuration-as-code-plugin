14575d07211a6710a1d5ad410cb435c78f4fa1ce
Baptiste Mathus <batmat@batmat.net>
2018-06-29 15:33:57 +0200

    Add a test demonstrating EC2 configuration
    This works almost entirely.
    The current code fails while checking it can get the `labels` back.
    For some reason, the labels seems to get set, but is not stored.
    
    And I can confirm that doesn't work in the UI, since I wrote this
    test after wondering why everything seemed fine, but that field
    stayed empty on the UI.

---
+++ integrations/src/test/java/org/jenkinsci/plugins/casc/EC2CloudTest.java
@@ -0,0 +1,42 @@
+package org.jenkinsci.plugins.casc;
+
+import hudson.plugins.ec2.AmazonEC2Cloud;
+import hudson.plugins.ec2.SlaveTemplate;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.casc.misc.ConfiguredWithCode;
+import org.jenkinsci.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class EC2CloudTest {
+
+    @Rule
+    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    @ConfiguredWithCode("EC2Cloud.yml")
+    public void configure_ec2_cloud() throws Exception {
+        final AmazonEC2Cloud ec2Cloud = (AmazonEC2Cloud) Jenkins.getInstance().getCloud("ec2-ec2");
+        assertNotNull(ec2Cloud);
+
+        assertTrue(ec2Cloud.isUseInstanceProfileForCredentials());
+        final List<SlaveTemplate> templates = ec2Cloud.getTemplates();
+        assertThat(templates, hasSize(1));
+        final SlaveTemplate slaveTemplate = templates.get(0);
+        assertThat(slaveTemplate.getAmi(), equalTo("ami-0c6bb742864ffa3f3"));
+
+        assertThat(slaveTemplate.getLabelString(), containsString("test"));
+        assertThat(slaveTemplate.getLabelString(), containsString("yey"));
+    }
+
+
+}
