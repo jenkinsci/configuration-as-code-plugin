2eee696c8862f714ca87f13343b47c2d5dc9ef61
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-08-06 12:19:20 +0200

    introduce "support" module for plugin-specific adapters
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ support/src/main/java/org/jenkinsci/plugins/casc/support/jobdsl/FromFileScriptSource.java
@@ -0,0 +1,34 @@
+package org.jenkinsci.plugins.casc.support.jobdsl;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import org.apache.commons.io.FileUtils;
+import org.jenkinsci.Symbol;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class FromFileScriptSource extends ConfigurableScriptSource {
+
+    public String path;
+
+    @Override
+    public void configure(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String getScript() throws IOException {
+        return FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
+    }
+
+    @Extension
+    @Symbol("file")
+    public static class DescriptorImpl extends Descriptor<ScriptSource> {
+
+    }
+}
