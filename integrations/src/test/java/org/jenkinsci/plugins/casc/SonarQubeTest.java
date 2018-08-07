21a7da71e12162225a1911eb84d319703518006b
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-08-07 10:19:16 +0200

    test case for sonarQube plugin
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ integrations/src/test/java/org/jenkinsci/plugins/casc/SonarQubeTest.java
@@ -0,0 +1,40 @@
+package org.jenkinsci.plugins.casc;
+
+import hudson.plugins.sonar.SonarGlobalConfiguration;
+import hudson.plugins.sonar.SonarInstallation;
+import hudson.plugins.sonar.model.TriggersConfig;
+import jenkins.model.GlobalConfiguration;
+import org.jenkinsci.plugins.casc.misc.ConfiguredWithCode;
+import org.jenkinsci.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class SonarQubeTest {
+
+    @Rule
+    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+
+    @Test
+    @ConfiguredWithCode("SonarQubeTest.yml")
+    public void configure_sonar_globalconfig() throws Exception {
+
+        final SonarGlobalConfiguration configuration = GlobalConfiguration.all().get(SonarGlobalConfiguration.class);
+        assertTrue(configuration.isBuildWrapperEnabled());
+        final SonarInstallation installation = configuration.getInstallations()[0];
+        assertEquals("TEST", installation.getName());
+        assertEquals("http://url:9000", installation.getServerUrl());
+        assertEquals("token", installation.getServerAuthenticationToken());
+        assertEquals("mojoVersion", installation.getMojoVersion());
+        assertEquals("additionalAnalysisProperties", installation.getAdditionalAnalysisProperties());
+        final TriggersConfig triggers = installation.getTriggers();
+        assertTrue(triggers.isSkipScmCause());
+        assertTrue(triggers.isSkipUpstreamCause());
+        assertEquals("envVar", triggers.getEnvVar());
+    }
+
+}
