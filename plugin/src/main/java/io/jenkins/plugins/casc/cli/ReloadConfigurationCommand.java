9f7f8a96a5964f1360f9e0240af61f9b7c84f197
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-08-29 15:46:27 +0200

    CLI command to reload configuration
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ plugin/src/main/java/io/jenkins/plugins/casc/cli/ReloadConfigurationCommand.java
@@ -0,0 +1,35 @@
+package io.jenkins.plugins.casc.cli;
+
+import hudson.Extension;
+import hudson.cli.CLICommand;
+import io.jenkins.plugins.casc.ConfigurationAsCode;
+import io.jenkins.plugins.casc.yaml.YamlSource;
+import jenkins.model.Jenkins;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
+import java.io.InputStream;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+@Extension
+@Restricted(NoExternalUse.class)
+public class ReloadConfigurationCommand extends CLICommand {
+
+    @Override
+    public String getShortDescription() {
+        return "Reload YAML configuration";
+    }
+
+    @Override
+    protected int run() throws Exception {
+
+        if (!Jenkins.getInstance().hasPermission(Jenkins.ADMINISTER)) {
+            return -1;
+        }
+
+        ConfigurationAsCode.get().configure();
+        return 0;
+    }
+}
