163ecf263b565ddc62a718c84d15ed08c0ffb393
Olivier Dagenais <odagenais@kinaxis.com>
2018-11-15 11:21:07 -0500

    feat: add skeleton MavenExporter[Test]

---
+++ plugin/src/main/java/io/jenkins/plugins/casc/plugins/MavenExporter.java
@@ -0,0 +1,8 @@
+package io.jenkins.plugins.casc.plugins;
+
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
+@Restricted(NoExternalUse.class)
+public class MavenExporter {
+}
---
+++ plugin/src/test/java/io/jenkins/plugins/casc/plugins/MavenExporterTest.java
@@ -0,0 +1,11 @@
+package io.jenkins.plugins.casc.plugins;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A class to test {@link MavenExporter}.
+ */
+public class MavenExporterTest {
+
+}
