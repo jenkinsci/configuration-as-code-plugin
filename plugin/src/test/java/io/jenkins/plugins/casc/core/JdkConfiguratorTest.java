8f6fc253d71f645ad474a25b191fcf21641f4ea3
Mike Glazer <mglazer@palantir.com>
2019-01-27 12:47:01 -0800

    Add example, remove commented code

--- plugin/src/test/java/io/jenkins/plugins/casc/core/JdkConfiguratorTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/core/JdkConfiguratorTest.java
@@ -29,13 +29,5 @@ public class JdkConfiguratorTest {
 
         InstallSourceProperty installSourceProperty = jdk.getProperties().get(InstallSourceProperty.class);
         Assert.assertEquals(1, installSourceProperty.installers.size());
-
-        // https://github.com/jenkinsci/jenkins/pull/3301 moved jdk-tool
-        // to a separate plugin, with no published JAR file
-        /*
-        JDKInstaller installer = installSourceProperty.installers.get(JDKInstaller.class);
-        Assert.assertEquals("jdk-8u181-oth-JPR", installer.id);
-        Assert.assertTrue(installer.acceptLicense);
-        */
     }
 }
