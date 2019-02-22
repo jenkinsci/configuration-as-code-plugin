fb682ae6c1f7bd4ccc2d4743297aa53197c9e0cc
Scott Grimes <scott-grimes@users.noreply.github.com>
2019-02-22 16:35:03 -0600

    optionally set vault engine in the vault config (#752)
    fixes #746

--- plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/VaultSecretSource.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/VaultSecretSource.java
@@ -50,6 +50,7 @@ public class VaultSecretSource extends SecretSource {
         String vaultAppRole = getVariable("CASC_VAULT_APPROLE", prop);
         String vaultAppRoleSecret = getVariable("CASC_VAULT_APPROLE_SECRET", prop);
         String vaultNamespace = getVariable("CASC_VAULT_NAMESPACE", prop);
+        String vaultEngineVersion = getVariable("CASC_VAULT_ENGINE_VERSION", prop);
 
         if(((vaultPw != null && vaultUsr != null) || 
             vaultToken != null || 
@@ -61,6 +62,11 @@ public class VaultSecretSource extends SecretSource {
                     // optionally set namespace
                     config = config.nameSpace(vaultNamespace);
                     LOGGER.log(Level.FINE, "Using namespace with Vault: {0}", vaultNamespace);
+                }
+                if (vaultEngineVersion != null) {
+                    // optionally set vault engine version
+                    config = config.engineVersion( Integer.parseInt(vaultEngineVersion) );
+                    LOGGER.log(Level.FINE, "Using engine version: {0}", vaultEngineVersion);
                 }
                 config = config.build();
                 Vault vault = new Vault(config);
