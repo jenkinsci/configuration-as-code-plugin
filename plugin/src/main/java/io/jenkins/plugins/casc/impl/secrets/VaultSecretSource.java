29e3c22c43f20c1afbb2db5b260bb0b79d6026bb
Scott Grimes <scott-grimes@users.noreply.github.com>
2019-02-22 15:55:06 -0600

    Update VaultSecretSource.java

--- plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/VaultSecretSource.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/VaultSecretSource.java
@@ -65,7 +65,7 @@ public class VaultSecretSource extends SecretSource {
                 }
                 if (vaultEngineVersion != null) {
                     // optionally set vault engine version
-                    config = config.setEngineVersion( Integer.parseInt(vaultEngineVersion) );
+                    config = config.engineVersion( Integer.parseInt(vaultEngineVersion) );
                     LOGGER.log(Level.FINE, "Using engine version: {0}", vaultEngineVersion);
                 }
                 config = config.build();
