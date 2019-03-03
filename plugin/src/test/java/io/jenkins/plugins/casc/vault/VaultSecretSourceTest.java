e20185608d809c560dbce726c4770200bfca1b1b
Karl Fischer <fishi0x01@users.noreply.github.com>
2019-03-03 19:39:13 +0000

    Add JUnit Tests with Vault Container (#764)

---
+++ plugin/src/test/java/io/jenkins/plugins/casc/vault/VaultSecretSourceTest.java
@@ -0,0 +1,125 @@
+package io.jenkins.plugins.casc.vault;
+
+
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.SecretSourceResolver;
+import org.junit.*;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.testcontainers.vault.VaultContainer;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static io.jenkins.plugins.casc.vault.VaultTestUtil.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
+
+// Inspired by https://github.com/BetterCloud/vault-java-driver/blob/master/src/test-integration/java/com/bettercloud/vault/util/VaultContainer.java
+public class VaultSecretSourceTest {
+
+    @ClassRule
+    public static VaultContainer vaultContainer = createVaultContainer();
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Rule
+    public EnvironmentVariables envVars = new EnvironmentVariables()
+        .set("CASC_VAULT_FILE", getClass().getResource("vaultTest_cascFile").getPath());
+
+    private ConfigurationContext context;
+
+    @BeforeClass
+    public static void configureContainer() {
+        // Check if docker daemon is available
+        assumeTrue(hasDockerDaemon());
+
+        // Create vault policies/users/roles ..
+        configureVaultContainer(vaultContainer);
+    }
+
+    @Before
+    public void refreshConfigurationContext() {
+        // Setup Jenkins
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        context = new ConfigurationContext(registry);
+    }
+
+    @Test
+    public void kv1WithUser() {
+        envVars.set("CASC_VAULT_USER", VAULT_USER);
+        envVars.set("CASC_VAULT_PW", VAULT_PW);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V1);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "1");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo("123"));
+    }
+
+    @Test
+    public void kv2WithUser() {
+        envVars.set("CASC_VAULT_USER", VAULT_USER);
+        envVars.set("CASC_VAULT_PW", VAULT_PW);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V2);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "2");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo("123"));
+    }
+
+    @Test
+    public void kv2WithWrongUser() {
+        envVars.set("CASC_VAULT_USER", "1234");
+        envVars.set("CASC_VAULT_PW", VAULT_PW);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V2);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "2");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo(""));
+    }
+
+    @Test
+    public void kv1WithToken() {
+        envVars.set("CASC_VAULT_TOKEN", VAULT_ROOT_TOKEN);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V1);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "1");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo("123"));
+    }
+
+    @Test
+    public void kv2WithToken() {
+        envVars.set("CASC_VAULT_TOKEN", VAULT_ROOT_TOKEN);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V2);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "2");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo("123"));
+    }
+
+    @Test
+    public void kv1WithWrongToken() {
+        envVars.set("CASC_VAULT_TOKEN", "1234");
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V1);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "1");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo(""));
+    }
+
+    @Test
+    public void kv1WithApprole() {
+        envVars.set("CASC_VAULT_APPROLE", VAULT_APPROLE_ID);
+        envVars.set("CASC_VAULT_APPROLE_SECRET", VAULT_APPROLE_SECRET);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V1);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "1");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo("123"));
+    }
+
+    @Test
+    public void kv2WithApprole() {
+        envVars.set("CASC_VAULT_APPROLE", VAULT_APPROLE_ID);
+        envVars.set("CASC_VAULT_APPROLE_SECRET", VAULT_APPROLE_SECRET);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V2);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "2");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo("123"));
+    }
+
+    @Test
+    public void kv2WithWrongApprole() {
+        envVars.set("CASC_VAULT_APPROLE", "1234");
+        envVars.set("CASC_VAULT_APPROLE_SECRET", VAULT_APPROLE_SECRET);
+        envVars.set("CASC_VAULT_PATH", VAULT_PATH_V2);
+        envVars.set("CASC_VAULT_ENGINE_VERSION", "2");
+        assertThat(SecretSourceResolver.resolve(context, "${key1}"), equalTo(""));
+    }
+}
---
+++ plugin/src/test/java/io/jenkins/plugins/casc/vault/VaultTestUtil.java
@@ -0,0 +1,93 @@
+package io.jenkins.plugins.casc.vault;
+
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.MountableFile;
+import org.testcontainers.utility.TestEnvironment;
+import org.testcontainers.vault.VaultContainer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SuppressWarnings("WeakerAccess")
+class VaultTestUtil {
+
+    private final static Logger LOGGER = Logger.getLogger(VaultTestUtil.class.getName());
+
+    private static final String VAULT_DOCKER_IMAGE = "vault:1.0.3";
+    public static final String VAULT_ROOT_TOKEN = "root-token";
+    public static final String VAULT_USER = "admin";
+    public static final String VAULT_PW = "admin";
+    public static final String VAULT_PATH_V1 = "kv-v1/admin";
+    public static final String VAULT_PATH_V2 = "kv-v2/admin";
+    public static String VAULT_APPROLE_ID = "";
+    public static String VAULT_APPROLE_SECRET = "";
+
+    private static void runCommand(VaultContainer container, final String... command)
+        throws IOException, InterruptedException {
+        LOGGER.log(Level.FINE, String.join(" ", command));
+        container.execInContainer(command);
+    }
+
+    public static boolean hasDockerDaemon() {
+        try {
+            return TestEnvironment.dockerApiAtLeast("1.10");
+        } catch (IllegalStateException e) {
+            return false;
+        }
+    }
+
+    public static VaultContainer createVaultContainer() {
+        if (!hasDockerDaemon()) return null;
+        return new VaultContainer<>(VaultTestUtil.VAULT_DOCKER_IMAGE)
+                .withVaultToken(VaultTestUtil.VAULT_ROOT_TOKEN)
+                .withCopyFileToContainer(MountableFile.forHostPath(
+                        VaultTestUtil.class.getResource("vaultTest_adminPolicy.hcl").getPath()),
+                        "/admin.hcl")
+                .withVaultPort(8200)
+                .waitingFor(Wait.forHttp("/v1/sys/seal-status").forStatusCode(200));
+    }
+
+    public static void configureVaultContainer(VaultContainer container) {
+        try {
+            // Create Secret Backends
+            runCommand(container, "vault", "secrets", "enable", "-path=kv-v2",
+                    "-version=2", "kv");
+            runCommand(container, "vault", "secrets", "enable", "-path=kv-v1",
+                    "-version=1", "kv");
+
+            // Create user/password credential
+            runCommand(container, "vault", "auth", "enable", "userpass");
+            runCommand(container, "vault", "write", "auth/userpass/users/" + VAULT_USER,
+                    "password=" + VAULT_PW, "policies=admin");
+
+            // Create policies
+            runCommand(container, "vault", "policy", "write", "admin", "/admin.hcl");
+
+            // Create AppRole
+            runCommand(container, "vault", "auth", "enable", "approle");
+            runCommand(container, "vault", "write", "auth/approle/role/admin",
+                    "secret_id_ttl=10m", "token_num_uses=0", "token_ttl=20m", "token_max_ttl=20m",
+                    "secret_id_num_uses=1000", "policies=admin");
+
+            // Retrieve AppRole credentials
+            VaultConfig config = new VaultConfig().address("http://localhost:8200")
+                    .token(VAULT_ROOT_TOKEN).engineVersion(1).build();
+            Vault vaultClient = new Vault(config);
+            VAULT_APPROLE_ID = vaultClient.logical().read("auth/approle/role/admin/role-id")
+                    .getData().get("role_id");
+            VAULT_APPROLE_SECRET = vaultClient.logical().write("auth/approle/role/admin/secret-id",
+                    new HashMap<>()).getData().get("secret_id");
+
+            // add secrets for v1 and v2
+            runCommand(container, "vault", "kv", "put", VAULT_PATH_V1, "key1=123", "key2=456");
+            runCommand(container, "vault", "kv", "put", VAULT_PATH_V2, "key1=123", "key2=456");
+
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, e.getMessage());
+        }
+    }
+}
