8727ad0782dae94c7d242b6ad1486de6c696a272
Karl Fischer <fishi0x01@gmail.com>
2019-03-05 19:01:36 +0100

    Fix NPEs

--- plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/VaultSecretSource.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/VaultSecretSource.java
@@ -157,68 +157,77 @@ public class VaultSecretSource extends SecretSource {
         Optional<String> vaultUserOpt = Optional.ofNullable(vaultUser);
         Optional<String> vaultPwOpt = Optional.ofNullable(vaultPw);
 
-        if (needNewToken()) {
-            Optional<String> authTokenOpt = Optional.empty();
+        if (vaultTokenOpt.isPresent() ||
+                (vaultAppRoleOpt.isPresent() && vaultAppRoleSecretOpt.isPresent()) ||
+                (vaultUserOpt.isPresent() && vaultPwOpt.isPresent())
+        ) {
+            if (needNewToken()) {
+                Optional<String> authTokenOpt = Optional.empty();
 
-            // attempt token login
-            if (vaultTokenOpt.isPresent()) {
-                authTokenOpt = vaultTokenOpt;
-                LOGGER.log(Level.FINE, "Using supplied token to access Vault");
-            }
-
-            // attempt AppRole login
-            if (vaultAppRoleOpt.isPresent() && vaultAppRoleSecretOpt.isPresent() && !authTokenOpt.isPresent()) {
-                try {
-                    authTokenOpt = Optional.ofNullable(
-                            vault.auth().loginByAppRole(vaultAppRoleOpt.get(), vaultAppRoleSecretOpt.get()).getAuthClientToken()
-                    );
-                    LOGGER.log(Level.FINE, "Login to Vault using AppRole/SecretID successful");
-                } catch (VaultException e) {
-                    LOGGER.log(Level.WARNING, "Could not login with AppRole", e);
+                // attempt token login
+                if (vaultTokenOpt.isPresent()) {
+                    authTokenOpt = vaultTokenOpt;
+                    LOGGER.log(Level.FINE, "Using supplied token to access Vault");
                 }
-            }
 
-            // attempt User/Pass login
-            if (vaultUserOpt.isPresent() && vaultPwOpt.isPresent() && !authTokenOpt.isPresent()) {
-                try {
-                    authTokenOpt = Optional.ofNullable(
-                            vault.auth().loginByUserPass(vaultUserOpt.get(), vaultPwOpt.get(), vaultMount).getAuthClientToken()
-                    );
-                    LOGGER.log(Level.FINE, "Login to Vault using User/Pass successful");
-                } catch (VaultException e) {
-                    LOGGER.log(Level.WARNING, "Could not login with User/Pass", e);
+                // attempt AppRole login
+                if (vaultAppRoleOpt.isPresent() && vaultAppRoleSecretOpt.isPresent() && !authTokenOpt.isPresent()) {
+                    try {
+                        authTokenOpt = Optional.ofNullable(
+                                vault.auth().loginByAppRole(vaultAppRoleOpt.get(), vaultAppRoleSecretOpt.get()).getAuthClientToken()
+                        );
+                        LOGGER.log(Level.FINE, "Login to Vault using AppRole/SecretID successful");
+                    } catch (VaultException e) {
+                        LOGGER.log(Level.WARNING, "Could not login with AppRole", e);
+                    }
                 }
-            }
 
-            if (authTokenOpt.isPresent()) {
-                setAuthTokenInVaultClient(authTokenOpt.get());
-            } else {
-                LOGGER.log(Level.WARNING, "Vault auth token is null.");
+                // attempt User/Pass login
+                if (vaultUserOpt.isPresent() && vaultPwOpt.isPresent() && !authTokenOpt.isPresent()) {
+                    try {
+                        authTokenOpt = Optional.ofNullable(
+                                vault.auth().loginByUserPass(vaultUserOpt.get(), vaultPwOpt.get(), vaultMount).getAuthClientToken()
+                        );
+                        LOGGER.log(Level.FINE, "Login to Vault using User/Pass successful");
+                    } catch (VaultException e) {
+                        LOGGER.log(Level.WARNING, "Could not login with User/Pass", e);
+                    }
+                }
+
+                if (authTokenOpt.isPresent()) {
+                    setAuthTokenInVaultClient(authTokenOpt.get());
+                } else {
+                    LOGGER.log(Level.WARNING, "Vault auth token is null.");
+                }
             }
         }
     }
 
     private void readSecretsFromVault() {
-        try {
-            // refresh map
-            secrets = new HashMap<>();
+        Optional<String[]> vaultPathsOpt = Optional.ofNullable(vaultPaths);
 
-            // Parse secrets
-            for (String vaultPath : vaultPaths) {
+        if (vaultPathsOpt.isPresent()) {
+            try {
+                // refresh map
+                secrets = new HashMap<>();
 
-                // check if we overwrite an existing key from another path
-                Map<String, String> nextSecrets = vault.logical().read(vaultPath).getData();
-                for (String key : nextSecrets.keySet()) {
-                    if (secrets.containsKey(key)) {
-                        LOGGER.log(Level.WARNING, "Key {0} exists in multiple vault paths.", key);
+                // Parse secrets
+                for (String vaultPath : vaultPathsOpt.get()) {
+
+                    // check if we overwrite an existing key from another path
+                    Map<String, String> nextSecrets = vault.logical().read(vaultPath).getData();
+                    for (String key : nextSecrets.keySet()) {
+                        if (secrets.containsKey(key)) {
+                            LOGGER.log(Level.WARNING, "Key {0} exists in multiple vault paths.", key);
+                        }
                     }
-                }
 
-                // merge
-                secrets.putAll(nextSecrets);
+                    // merge
+                    secrets.putAll(nextSecrets);
+                }
+            } catch (VaultException e) {
+                LOGGER.log(Level.WARNING, "Unable to fetch secret from Vault", e);
             }
-        } catch (VaultException e) {
-            LOGGER.log(Level.WARNING, "Unable to fetch secret from Vault", e);
         }
     }
 
