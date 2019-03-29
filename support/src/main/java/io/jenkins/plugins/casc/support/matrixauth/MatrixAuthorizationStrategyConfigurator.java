4ffe9921a841af34cc5f316d259e5d487a3dda62
Joseph Petersen <josephp90@gmail.com>
2019-03-29 02:00:58 +0100

    whoops

--- support/src/main/java/io/jenkins/plugins/casc/support/matrixauth/MatrixAuthorizationStrategyConfigurator.java
+++ support/src/main/java/io/jenkins/plugins/casc/support/matrixauth/MatrixAuthorizationStrategyConfigurator.java
@@ -21,7 +21,6 @@ import java.util.stream.Collectors;
 public abstract class MatrixAuthorizationStrategyConfigurator<T extends AuthorizationContainer> extends BaseConfigurator<T> {
 
     @NonNull
-    @CheckForNull
     @Override
     public Class getImplementedAPI() {
         return AuthorizationStrategy.class;
