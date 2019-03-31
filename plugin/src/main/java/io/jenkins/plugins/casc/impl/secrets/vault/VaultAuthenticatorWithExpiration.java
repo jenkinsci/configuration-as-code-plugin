846bcbde47ba5de664dc86745bde90bbd86a01a1
Joseph Petersen <josephp90@gmail.com>
2019-03-31 21:00:26 +0200

    whoops

--- plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/vault/VaultAuthenticatorWithExpiration.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/impl/secrets/vault/VaultAuthenticatorWithExpiration.java
@@ -1,7 +1,6 @@
 package io.jenkins.plugins.casc.impl.secrets.vault;
 
 import com.bettercloud.vault.Vault;
-import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
 import java.util.Calendar;
 import java.util.logging.Level;
