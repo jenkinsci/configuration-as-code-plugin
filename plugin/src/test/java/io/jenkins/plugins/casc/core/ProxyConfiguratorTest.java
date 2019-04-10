7255f818580359bc2fa02a0a7ee4db512ed5964d
Tim Jacomb <timjacomb1+github@gmail.com>
2019-04-10 20:59:10 +0100

    Add more test coverage for ProxyConfigurator

--- plugin/src/test/java/io/jenkins/plugins/casc/core/ProxyConfiguratorTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/core/ProxyConfiguratorTest.java
@@ -1,6 +1,8 @@
 package io.jenkins.plugins.casc.core;
 
 import hudson.ProxyConfiguration;
+import hudson.util.Secret;
+import io.jenkins.plugins.casc.ConfigurationAsCode;
 import io.jenkins.plugins.casc.ConfigurationContext;
 import io.jenkins.plugins.casc.Configurator;
 import io.jenkins.plugins.casc.ConfiguratorRegistry;
@@ -8,9 +10,14 @@ import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.model.CNode;
 import io.jenkins.plugins.casc.model.Mapping;
+import io.jenkins.plugins.casc.snakeyaml.nodes.Node;
+import java.io.IOException;
+import java.io.StringWriter;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static io.jenkins.plugins.casc.ConfigurationAsCode.serializeYamlNode;
+import static java.util.Objects.requireNonNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -28,7 +35,7 @@ public class ProxyConfiguratorTest {
         assertEquals(proxy.port, 80);
 
         assertEquals(proxy.getUserName(), "login");
-        assertEquals(proxy.getPassword(), "password");
+        assertEquals(Secret.decrypt(proxy.getEncryptedPassword()).getPlainText(), "password");
         assertEquals(proxy.noProxyHost, "externalhost");
         assertEquals(proxy.getTestUrl(), "http://google.com");
 
@@ -60,5 +67,62 @@ public class ProxyConfiguratorTest {
         Mapping mapping = node.asMapping();
         assertEquals(3, node.asMapping().size());
         assertEquals("proxyhost", mapping.getScalarValue("name"));
+        assertEquals("", Secret.decrypt(mapping.getScalarValue("password")).getPlainText());
+    }
+
+    @Test
+    @ConfiguredWithCode("Proxy.yml")
+    public void describeProxyConfig() throws Exception {
+        final JenkinsConfigurator root = getJenkinsConfigurator();
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        final CNode configNode = getProxyNode(root, context);
+
+        Secret password = requireNonNull(Secret.decrypt(getProxyNode(root, context).asMapping().getScalarValue("password")));
+
+        final String yamlConfig = toYamlString(configNode);
+        assertEquals(String.join("\n",
+                "name: \"proxyhost\"",
+                "noProxyHost: \"externalhost\"",
+                "password: \"" + password.getEncryptedValue() + "\"",
+                "port: 80",
+                "testUrl: \"http://google.com\"",
+                "userName: \"login\"",
+                ""
+        ), yamlConfig);
+    }
+
+    @Test
+    @ConfiguredWithCode("ProxyMinimal.yml")
+    public void describeMinimalProxyConfig() throws Exception {
+        final JenkinsConfigurator root = getJenkinsConfigurator();
+        ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+        ConfigurationContext context = new ConfigurationContext(registry);
+        final CNode configNode = getProxyNode(root, context);
+
+        Secret password = requireNonNull(Secret.decrypt(getProxyNode(root, context).asMapping().getScalarValue("password")));
+
+        final String yamlConfig = toYamlString(configNode);
+        assertEquals(String.join("\n",
+                "name: \"proxyhost\"",
+                "password: \"" + password.getEncryptedValue() + "\"", // It's an empty string here
+                "port: 80",
+                ""
+        ), yamlConfig);
+    }
+
+    private CNode getProxyNode(JenkinsConfigurator root, ConfigurationContext context) throws Exception {
+        return requireNonNull(root.describe(root.getTargetComponent(context), context)).asMapping().get("proxy");
+    }
+
+    private JenkinsConfigurator getJenkinsConfigurator() {
+        return j.jenkins.getExtensionList(JenkinsConfigurator.class).get(0);
+    }
+
+    private static String toYamlString(CNode rootNode) throws IOException {
+        Node yamlRoot = ConfigurationAsCode.get().toYaml(rootNode);
+        StringWriter buffer = new StringWriter();
+        serializeYamlNode(yamlRoot, buffer);
+        return buffer.toString();
     }
 }
