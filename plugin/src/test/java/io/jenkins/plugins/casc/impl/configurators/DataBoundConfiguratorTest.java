5942da61101fc3edeea41b5fb3464005fe09df29
Tim Jacomb <timjacomb1+github@gmail.com>
2019-06-28 08:54:25 +0100

    Shush codacy

--- plugin/src/test/java/io/jenkins/plugins/casc/impl/configurators/DataBoundConfiguratorTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/impl/configurators/DataBoundConfiguratorTest.java
@@ -126,7 +126,7 @@ public class DataBoundConfiguratorTest {
             this.bar = bar;
             this.qix = qix;
             if (qix == 99) {
-                throw new NullPointerException("Magic test fail");
+                throw new RuntimeException("Magic test fail");
             }
         }
 
