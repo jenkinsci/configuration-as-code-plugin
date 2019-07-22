5e5adf1fa3a3381184f8547b47349871c9ce256c
Oleg Nenashev <o.v.nenashev@gmail.com>
2019-07-22 15:57:34 +0200

    [JENKINS-52906] - Update JIRA Plugin test dependency to verify the JCasC compatibility and support writing roundtrip tests (#956)
    * [JENKINS-52906] - Update JIRA Plugin test dependency to verify the export/import fixes
    
    * [JENKINS-52906] - Remove the unused import
    
    * Update integrations/src/test/resources/io/jenkins/plugins/casc/JiraTest.yml
    
    Co-Authored-By: Joseph Petersen <josephp90@gmail.com>

--- integrations/src/test/java/io/jenkins/plugins/casc/JiraTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/JiraTest.java
@@ -2,11 +2,11 @@ package io.jenkins.plugins.casc;
 
 import hudson.plugins.jira.JiraProjectProperty;
 import hudson.plugins.jira.JiraProjectProperty.DescriptorImpl;
-import hudson.plugins.jira.JiraSite;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,19 +20,15 @@ public class JiraTest {
 
     @Test
     @ConfiguredWithCode("JiraTest.yml")
+    @Issue("JENKINS-52906")
     public void configure_jira_project_globalconfig() throws Exception {
 
         final DescriptorImpl descriptor = (DescriptorImpl) j.jenkins.getDescriptor(JiraProjectProperty.class);
 
-        /*  JENKINS-52906
-         *  assertEquals(2, descriptor.getSites().length);
-         *  assertEquals("http://jira.codehaus.org/", sites[0].getUrl().toString());
-         *  assertEquals("http://issues.jenkins.io/", sites[1].getUrl().toString());
-         */
-
-        assertEquals(1, descriptor.getSites().length);
-        final JiraSite site = descriptor.getSites()[0];
-        assertEquals("http://jira.codehaus.org/", site.getUrl().toString());
+        // Was failing due to JENKINS-52906
+        assertEquals(2, descriptor.getSites().length);
+        assertEquals("http://jira.codehaus.org/", descriptor.getSites()[0].getUrl().toString());
+        assertEquals("http://issues.jenkins.io/", descriptor.getSites()[1].getUrl().toString());
     }
 
 }
