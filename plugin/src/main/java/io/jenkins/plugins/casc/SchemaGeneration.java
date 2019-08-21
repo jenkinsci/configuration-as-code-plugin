0271cd3d133d8b7407ba5acdd468303a37727ba4
Sladyn <gunnerforlife00@gmail.com>
2019-08-21 09:47:23 +0530

    Completed the string generation 2/4 of the schema

--- plugin/src/main/java/io/jenkins/plugins/casc/SchemaGeneration.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/SchemaGeneration.java
@@ -1,21 +1,127 @@
 package io.jenkins.plugins.casc;
 
-
+import hudson.DescriptorExtensionList;
+import io.jenkins.plugins.casc.impl.configurators.DataBoundConfigurator;
+import io.jenkins.plugins.casc.impl.configurators.HeteroDescribableConfigurator;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.List;
+import jenkins.model.Jenkins;
 
 public class SchemaGeneration {
 
-
     public String generateSchema() {
 
-            /*
-            *
-            * The Code to generate the schema needs to be attached here.
-            *
-            *
-            *
-            * */
+        LinkedHashSet linkedHashSet = new LinkedHashSet<>(ConfigurationAsCode.get().getRootConfigurators());
+        Iterator<RootElementConfigurator> i = linkedHashSet.iterator();
 
-            return null;
+        StringBuilder schemaString = new StringBuilder();
+        schemaString.append("{\n" +
+                "  \"$schema\": \"http://json-schema.org/draft-06/schema#\",\n" +
+                "  \"id\": \"http://jenkins.io/configuration-as-code#\",\n" +
+                "  \"description\": \"Jenkins Configuration as Code\",\n" +
+                "  \"type\": \"object\",\n" +
+                "  \"properties\": {\n" +
+                "");
+
+        while (i.hasNext()) {
+
+            List<Attribute> attributeList = new ArrayList<>(i.next().getAttributes());
+            for (Attribute a : attributeList) {
+                String type;
+                if (a.type.toString().equals("int")) {
+                    type = "int";
+                } else if (a.type.toString().equals("class java.lang.String")) {
+                    type = "string";
+                } else if (a.type.toString().equals("boolean")) {
+                    type = "boolean";
+                } else {
+                    type = "object";
+                }
+                schemaString.append(a.name + ": " + "{\n" +
+                        "      \"type\": \"" + type + "\",\n" +
+                        "      \"$ref\": \"#/definitions/" + a.type.getName() + "\"\n" +
+                        "    },");
+            }
+        }
+        System.out.println(schemaString);
+
+        /**
+         * Used to generate the schema for the descriptors
+         * Finds out the instance of each of the configurators
+         * and gets the required descriptors from the instance method.
+         * Appending the oneOf tag to the schema.
+         */
+
+        schemaString.append("schema\" : {\n" +
+                "    \"oneOf\": [");
+
+        ConfigurationAsCode configurationAsCode = ConfigurationAsCode.get();
+        configurationAsCode.getConfigurators()
+                .stream()
+                .forEach(configurator -> {
+
+                    DescriptorExtensionList descriptorExtensionList = null;
+                    System.out.println(configurator.getClass().getName());
+                    if(configurator instanceof HeteroDescribableConfigurator) {
+                        HeteroDescribableConfigurator heteroDescribableConfigurator = (HeteroDescribableConfigurator) configurator;
+                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(heteroDescribableConfigurator.getTarget());
+
+                    } else if (configurator instanceof DataBoundConfigurator) {
+                        DataBoundConfigurator dataBoundConfigurator = (DataBoundConfigurator) configurator;
+                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(dataBoundConfigurator.getTarget());
+                    }
+
+//                    }  else if (configurator instanceof JenkinsConfigurator) {
+//                        JenkinsConfigurator jenkinsConfigurator = (JenkinsConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(jenkinsConfigurator.getTarget());
+//
+//                    }  else if (configurator instanceof GlobalConfigurationCategoryConfigurator) {
+//                        GlobalConfigurationCategoryConfigurator globalConfigurationCategoryConfigurator = (GlobalConfigurationCategoryConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(globalConfigurationCategoryConfigurator.getTarget());
+//
+//                    }  else if (configurator instanceof UnsecuredAuthorizationStrategyConfigurator) {
+//                        UnsecuredAuthorizationStrategyConfigurator unsecuredAuthorizationStrategyConfigurator = (UnsecuredAuthorizationStrategyConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(unsecuredAuthorizationStrategyConfigurator.getTarget());
+//
+//                    } else if (configurator instanceof UpdateCenterConfigurator) {
+//                        UpdateCenterConfigurator updateCenterConfigurator = (UpdateCenterConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(updateCenterConfigurator.getTarget());
+//
+//                    } else if (configurator instanceof HudsonPrivateSecurityRealmConfigurator) {
+//                        HudsonPrivateSecurityRealmConfigurator hudsonPConfigurator = (HudsonPrivateSecurityRealmConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(hudsonPConfigurator.getTarget());
+//
+//                    } else if (configurator instanceof UpdateSiteConfigurator) {
+//                        UpdateSiteConfigurator updateSiteConfigurator = (UpdateSiteConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(updateSiteConfigurator.getTarget());
+//
+//                    } else if (configurator instanceof JNLPLauncherConfigurator) {
+//                        JNLPLauncherConfigurator jLaunchConfigurator = (JNLPLauncherConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(jLaunchConfigurator.getTarget());
+//
+//                    } else if (configurator instanceof AdminWhitelistRuleConfigurator) {
+//                        AdminWhitelistRuleConfigurator adminWhitelistRuleConfigurator = (AdminWhitelistRuleConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(adminWhitelistRuleConfigurator.getTarget());
+//
+//                    } else if(configurator instanceof MavenConfigurator ) {
+//                        MavenConfigurator mavenConfigurator = (MavenConfigurator) configurator;
+//                        descriptorExtensionList = Jenkins.getInstance().getDescriptorList(mavenConfigurator.getTarget());
+//                    }
+                    /**
+                     * Iterate over the list and generate the schema
+                     */
+
+                    for (Object obj: descriptorExtensionList) {
+                        schemaString.append("{\n" +
+                                "      \"properties\" : {" +
+                                "      \"" + obj.getClass().getName() +"\"" + ": { \"$ref\" : \"#/definitions/" +
+                                obj.toString() + "\" }" +
+                                " }");
+                    }
+                });
+
+            return schemaString.toString();
     }
 }
--- plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
@@ -1,19 +1,8 @@
 package io.jenkins.plugins.casc;
 
-import hudson.DescriptorExtensionList;
-import hudson.model.UpdateSite;
-import hudson.util.DirScanner;
-import io.jenkins.plugins.casc.core.*;
-import io.jenkins.plugins.casc.impl.configurators.DataBoundConfigurator;
-import io.jenkins.plugins.casc.impl.configurators.GlobalConfigurationCategoryConfigurator;
-import io.jenkins.plugins.casc.impl.configurators.HeteroDescribableConfigurator;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import org.junit.Rule;
 import org.junit.Test;
-import java.util.*;
-
-
-import java.util.LinkedHashSet;
 
 
 public class SchemaGenerationTest{
@@ -26,105 +15,12 @@ public class SchemaGenerationTest{
     public void schemaShouldSucceed() {
 
 
-        /**
-         * Used to generate the schema for root configurators
-         */
-        LinkedHashSet linkedHashSet = new LinkedHashSet<>(ConfigurationAsCode.get().getRootConfigurators());
-        Iterator<RootElementConfigurator> i = linkedHashSet.iterator();
-
-        StringBuilder schemaString = new StringBuilder();
-        schemaString.append("{\n" +
-                "  \"$schema\": \"http://json-schema.org/draft-06/schema#\",\n" +
-                "  \"id\": \"http://jenkins.io/configuration-as-code#\",\n" +
-                "  \"description\": \"Jenkins Configuration as Code\",\n" +
-                "  \"type\": \"object\",\n" +
-                "  \"properties\": {\n" +
-                "");
-
-        while (i.hasNext()) {
-
-            List<Attribute> attributeList = new ArrayList<>(i.next().getAttributes());
-            for (Attribute a : attributeList) {
-                String type;
-                if (a.type.toString().equals("int")) {
-                    type = "int";
-                } else if (a.type.toString().equals("class java.lang.String")) {
-                    type = "string";
-                } else if (a.type.toString().equals("boolean")) {
-                    type = "boolean";
-                } else {
-                    type = "object";
-                }
-                schemaString.append(a.name + ": " + "{\n" +
-                        "      \"type\": \"" + type + "\",\n" +
-                        "      \"$ref\": \"#/definitions/" + a.type.getName() + "\"\n" +
-                        "    },");
-            }
-        }
-//        System.out.println(schemaString);
 
         /**
-         * Used to generate the schema for the descriptors
-         * Finds out the instance of each of the configurators
-         * and gets the required descriptors from the instance method.
+         *Validate the schema against a validator
+         * or against the already defined schema.
          */
-        ConfigurationAsCode configurationAsCode = ConfigurationAsCode.get();
-        configurationAsCode.getConfigurators()
-                .stream()
-                .forEach(configurator -> {
 
-                    DescriptorExtensionList descriptorExtensionList = null;
-
-                    if(configurator instanceof HeteroDescribableConfigurator) {
-                        HeteroDescribableConfigurator heteroDescribableConfigurator = (HeteroDescribableConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(heteroDescribableConfigurator.getTarget());
-
-                    } else if (configurator instanceof DataBoundConfigurator){
-                        DataBoundConfigurator dataBoundConfigurator = (DataBoundConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(dataBoundConfigurator.getTarget());
-
-
-                    }  else if (configurator instanceof JenkinsConfigurator) {
-                        JenkinsConfigurator jenkinsConfigurator = (JenkinsConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(jenkinsConfigurator.getTarget());
-
-                    }  else if (configurator instanceof GlobalConfigurationCategoryConfigurator) {
-                        GlobalConfigurationCategoryConfigurator globalConfigurationCategoryConfigurator = (GlobalConfigurationCategoryConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(globalConfigurationCategoryConfigurator.getTarget());
-
-                    }  else if (configurator instanceof UnsecuredAuthorizationStrategyConfigurator) {
-                        UnsecuredAuthorizationStrategyConfigurator unsecuredAuthorizationStrategyConfigurator = (UnsecuredAuthorizationStrategyConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(unsecuredAuthorizationStrategyConfigurator.getTarget());
-
-                    } else if (configurator instanceof UpdateCenterConfigurator) {
-                        UpdateCenterConfigurator updateCenterConfigurator = (UpdateCenterConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(updateCenterConfigurator.getTarget());
-
-                    } else if (configurator instanceof HudsonPrivateSecurityRealmConfigurator) {
-                        HudsonPrivateSecurityRealmConfigurator hudsonPConfigurator = (HudsonPrivateSecurityRealmConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(hudsonPConfigurator.getTarget());
-
-                    } else if (configurator instanceof UpdateSiteConfigurator) {
-                        UpdateSiteConfigurator updateSiteConfigurator = (UpdateSiteConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(updateSiteConfigurator.getTarget());
-
-                    } else if (configurator instanceof JNLPLauncherConfigurator) {
-                        JNLPLauncherConfigurator jLaunchConfigurator = (JNLPLauncherConfigurator) configurator;
-                        descriptorExtensionList = j.jenkins.getDescriptorList(jLaunchConfigurator.getTarget());
-                    }
-
-                    /**
-                     * Iterate over the list and generate the schema
-                     */
-
-//
-                    for (Object obj: descriptorExtensionList) {
-                        /*
-                         * Iterate over the obj list and construct the schema.
-                         * */
-                    }
-                });
 
     }
-
 }
