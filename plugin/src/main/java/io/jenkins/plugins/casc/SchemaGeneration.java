4657e0c7bcb8fbe4572cf98379b6c7b35720c520
Sladyn <gunnerforlife00@gmail.com>
2019-08-24 17:52:54 +0530

    Added validation initial test

--- plugin/src/main/java/io/jenkins/plugins/casc/SchemaGeneration.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/SchemaGeneration.java
@@ -1,14 +1,16 @@
 package io.jenkins.plugins.casc;
 
-import java.util.*;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import io.jenkins.plugins.casc.impl.configurators.HeteroDescribableConfigurator;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 
 public class SchemaGeneration {
+
 
     public static String generateSchema() {
 
@@ -17,26 +19,27 @@ public class SchemaGeneration {
          */
         StringBuilder schemaString = new StringBuilder();
         schemaString.append("{\n" +
-                "  \"$schema\": \"http://json-schema.org/draft-06/schema#\",\n" +
-                "  \"id\": \"http://jenkins.io/configuration-as-code#\",\n" +
-                "  \"description\": \"Jenkins Configuration as Code\",\n" +
-                "  \"type\": \"object\",\n" +
-                "  \"properties\": {\n" +
-                "");
+            "  \"$schema\": \"http://json-schema.org/draft-06/schema#\",\n" +
+            "  \"id\": \"http://jenkins.io/configuration-as-code#\",\n" +
+            "  \"description\": \"Jenkins Configuration as Code\",\n" +
+            "  \"type\": \"object\",\n" +
+            "  \"properties\": {\n" +
+            "");
 
         /**
          * This generates the schema for the root configurators
          * Iterates over the root elements and adds them to the schema.
          */
-        LinkedHashSet linkedHashSet = new LinkedHashSet<>(ConfigurationAsCode.get().getRootConfigurators());
+        LinkedHashSet linkedHashSet = new LinkedHashSet<>(
+            ConfigurationAsCode.get().getRootConfigurators());
         Iterator<RootElementConfigurator> i = linkedHashSet.iterator();
         while (i.hasNext()) {
             RootElementConfigurator rootElementConfigurator = i.next();
-            schemaString.append("\"" + rootElementConfigurator.getName() + "\": {" + "\"type\": \"object\",\n" +
+            schemaString.append(
+                "\"" + rootElementConfigurator.getName() + "\": {" + "\"type\": \"object\",\n" +
                     "    },");
         }
         schemaString.append("},\n\"definitions\": {");
-
 
         /**
          * This generates the schema for the base configurators
@@ -44,113 +47,117 @@ public class SchemaGeneration {
          */
 
         ConfigurationAsCode configurationAsCodeObject = ConfigurationAsCode.get();
-          for (Object configuratorObject : configurationAsCodeObject.getConfigurators()) {
-                if (configuratorObject instanceof BaseConfigurator) {
-                    BaseConfigurator baseConfigurator = (BaseConfigurator) configuratorObject;
-                    List<Attribute> baseConfigAttributeList = baseConfigurator.getAttributes();
-                    if(baseConfigAttributeList.size() == 0 ) {
-                         schemaString.append("\"" + ((BaseConfigurator) configuratorObject).getTarget().toString() +
-                        "\":{" + "\"type\": \"object\"," + "\"properties\": {}},");
-                    } else {
-                        for (Attribute attribute:baseConfigAttributeList) {
-                            if(attribute.multiple) {
-                                schemaString.append("\"" + attribute.getName() + "\":");
+        for (Object configuratorObject : configurationAsCodeObject.getConfigurators()) {
+            if (configuratorObject instanceof BaseConfigurator) {
+                BaseConfigurator baseConfigurator = (BaseConfigurator) configuratorObject;
+                List<Attribute> baseConfigAttributeList = baseConfigurator.getAttributes();
+                if (baseConfigAttributeList.size() == 0) {
+                    schemaString.append(
+                        "\"" + ((BaseConfigurator) configuratorObject).getTarget().toString() +
+                            "\":{" + "\"type\": \"object\"," + "\"properties\": {}},");
+                } else {
+                    for (Attribute attribute : baseConfigAttributeList) {
+                        if (attribute.multiple) {
+                            schemaString.append("\"" + attribute.getName() + "\":");
 
-                                if(attribute.type.getName().equals("java.lang.String")) {
-                                    schemaString.append("{\"type\": \"string\"},");
+                            if (attribute.type.getName().equals("java.lang.String")) {
+                                schemaString.append("{\"type\": \"string\"},");
+                            } else {
+                                schemaString.append("{\"type\":  \"object\"," +
+                                    "\"$ref\": \"#/definitions/" + attribute.type.getName()
+                                    + "\"},");
+                            }
+                        } else {
+                            if (attribute.type.isEnum()) {
+                                if (attribute.type.getEnumConstants().length == 0) {
+                                    schemaString.append("\"" + attribute.getName() + "\":" + " {" +
+                                        "\"type\": \"string\"}");
                                 } else {
-                                     schemaString.append("{\"type\":  \"object\"," +
-                                            "\"$ref\": \"#/definitions/" + attribute.type.getName() + "\"},");
+                                    schemaString.append("\"" + attribute.getName() + "\":" + " {" +
+                                        "\"type\": \"string\"," + "\"enum\": [");
+
+                                    for (Object obj : attribute.type.getEnumConstants()) {
+                                        schemaString.append("\"" + obj + "\",");
+                                    }
+                                    schemaString.append("]},");
                                 }
                             } else {
-                                if(attribute.type.isEnum()) {
-                                    if(attribute.type.getEnumConstants().length == 0){
-                                        schemaString.append("\"" + attribute.getName() + "\":" + " {" +
-                                                "\"type\": \"string\"}");
-                                    }
-                                    else {
-                                        schemaString.append("\"" + attribute.getName() + "\":" + " {" +
-                                                "\"type\": \"string\"," + "\"enum\": [");
+                                schemaString.append("\"" + attribute.getName() + "\":");
+                                switch (attribute.type.getName()) {
 
-                                        for (Object obj : attribute.type.getEnumConstants()) {
-                                            schemaString.append("\"" + obj + "\",");
-                                        }
-                                         schemaString.append("]},");
-                                    }
-                                } else {
-                                    schemaString.append("\"" + attribute.getName() + "\":");
-                                    switch (attribute.type.getName()){
+                                    case "java.lang.String":
+                                        schemaString.append("{\n\"type\": \"string\"},\n");
+                                        break;
 
-                                        case "java.lang.String":
-                                            schemaString.append("{\n\"type\": \"string\"},\n");
-                                            break;
+                                    case "int":
+                                        schemaString.append("{\n\"type\": \"integer\"},\n");
+                                        break;
 
-                                        case "int":
-                                            schemaString.append("{\n\"type\": \"integer\"},\n");
-                                            break;
+                                    case "boolean":
+                                        schemaString.append("{\n\"type\": \"boolean\"},\n");
+                                        break;
 
-                                        case "boolean":
-                                             schemaString.append("{\n\"type\": \"boolean\"},\n");
-                                            break;
+                                    case "java.lang.Boolean":
+                                        schemaString.append("{\"type\": \"boolean\"},\n");
+                                        break;
 
-                                        case "java.lang.Boolean":
-                                            schemaString.append("{\"type\": \"boolean\"},\n");
-                                            break;
+                                    case "java.lang.Integer":
+                                        schemaString.append("{\n\"type\": \"integer\"},\n");
+                                        break;
 
-                                        case "java.lang.Integer":
-                                            schemaString.append("{\n\"type\": \"integer\"},\n");
-                                            break;
+                                    case "java.lang.Long":
+                                        schemaString.append("{\n\"type\": \"integer\"},\n");
+                                        break;
 
-                                        case "java.lang.Long":
-                                            schemaString.append("{\n\"type\": \"integer\"},\n");
-                                            break;
-
-                                        default:
-                                            schemaString.append("{\n\"type\":  \"object\",\n" +
-                                                    "\"$ref\": \"#/definitions/" + attribute.type.getName() + "\"},\n");
-                                            break;
-                                    }
-
+                                    default:
+                                        schemaString.append("{\n\"type\":  \"object\",\n" +
+                                            "\"$ref\": \"#/definitions/" + attribute.type.getName()
+                                            + "\"},\n");
+                                        break;
                                 }
+
                             }
                         }
                     }
                 }
-          }
+            }
+            /**
+             * Used to generate the schema for the implementors of
+             * the HetroDescribable Configurator
+             * It mimics the HetroDescribable Configurator.jelly
+             */
 
-        /**
-         * Used to generate the schema for the implementors of
-         * the HetroDescribable Configurator
-         * It mimics the HetroDescribable Configurator
-         */
-        ConfigurationAsCode configurationAsCode = ConfigurationAsCode.get();
-        configurationAsCode.getConfigurators()
-                .stream()
-                .forEach(configurator -> {
-                    if(configurator instanceof HeteroDescribableConfigurator) {
-                        HeteroDescribableConfigurator heteroDescribableConfigurator = (HeteroDescribableConfigurator) configurator;
-                        Map<String,Class> implementorsMap = heteroDescribableConfigurator.getImplementors();
+            else if (configuratorObject instanceof HeteroDescribableConfigurator) {
 
-                        if(implementorsMap.size() != 0) {
-                            schemaString.append("\"" + heteroDescribableConfigurator.getTarget().getName() + "\": {\n" +
-                                    " \"type\": \"object\"\n" +
-                                            "    ," + "\"oneOf\" : [");
-                            Iterator<Map.Entry<String, Class>> itr = implementorsMap.entrySet().iterator();
-                            while (itr.hasNext()) {
-                                Map.Entry<String, Class> entry = itr.next();
-                                schemaString.append("{\n" +
-                                        "      \"properties\" : {\n" +
-                                        "        \"" + entry.getKey() + "\" : { \"$ref\" : \"#/definitions/ " + entry.getValue() + "\" }\n" +
-                                        "      }\n" +
-                                        "    }\n" +
-                                        "    ,");
-                            }
-                            schemaString.append("]\n" + "  \n" + "  }\n" + ",");
-                        }
+                HeteroDescribableConfigurator heteroDescribableConfigurator = (HeteroDescribableConfigurator) configuratorObject;
+                Map<String, Class> implementorsMap = heteroDescribableConfigurator
+                    .getImplementors();
+
+                if (implementorsMap.size() != 0) {
+                    schemaString.append(
+                        "\"" + heteroDescribableConfigurator.getTarget().getName() + "\": {\n" +
+                            " \"type\": \"object\"\n" +
+                            "    ," + "\"oneOf\" : [");
+                    Iterator<Map.Entry<String, Class>> itr = implementorsMap.entrySet().iterator();
+                    while (itr.hasNext()) {
+                        Map.Entry<String, Class> entry = itr.next();
+                        schemaString.append("{\n" +
+                            "      \"properties\" : {\n" +
+                            "        \"" + entry.getKey() + "\" : { \"$ref\" : \"#/definitions/ "
+                            + entry.getValue() + "\" }\n" +
+                            "      }\n" +
+                            "    }\n" +
+                            "    ,");
                     }
-                });
+                    schemaString.append("]\n" + "  \n" + "  }\n" + ",");
+                }
+
+            }
+        }
 
         schemaString.append("}\n}");
         return schemaString.toString();
     }
+
+
 }
--- plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
@@ -1,8 +1,15 @@
 package io.jenkins.plugins.casc;
 
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.WebResponse;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.misc.Util;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -21,46 +28,44 @@ public class SchemaGenerationTest{
     public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
     @Test
-    public void schemaShouldSucceed() throws IOException {
-
-        /**
-         *Validate the schema against a validator
-         * or against the already defined schema.
-         */
-        String s = generateSchema();
-        System.out.println(s);
-        String fileName = "Schema";
-        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-        writer.write(s);
-        writer.close();
-    }
-
-    @Test
     public void downloadOldSchema() throws Exception {
 
-        String fileName = "OldSchema";
         JenkinsRule.WebClient client = j.createWebClient();
         WebRequest request = new WebRequest(client.createCrumbedUrl("configuration-as-code/schema"), POST);
         WebResponse response = client.loadWebResponse(request);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-        writer.write(response.getContentAsString()
-        );
-        writer.close();
+        System.out.println(response);
     }
 
 
     @Test
-    public void validateSchema() {
+    public void validateSchema() throws IOException {
+        String schema = generateSchema();
+        JSONObject jsonSchema = new JSONObject(
+            new JSONTokener(schema));
 
+        String answer = "";
+        try {
+            answer = Util.toStringFromYamlFile(this, "merge3.yml");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(answer);
     }
 
-    @Test
-    public void checkRootConfigurators() {
 
-    }
+//    @Test
+//    public void checkRootConfigurators() {
+//
+//
+//    }
+//
+//    @Test
+//    public void checkInitialTemplate() {
+//
+//    }
 
-    @Test
-    public void checkInitialTemplate() {
 
-    }
 }
