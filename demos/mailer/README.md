75c0c244f0947e73c586466dd0e352597977b4c9
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-16 19:48:02 +0100

    mailer using Readme Rule and markdown. Disabled deprecated method in the assert

--- demos/mailer/README.md
+++ demos/mailer/README.md
@@ -1,9 +1,14 @@
 # mailer plugin
 
-## Requires `mailer-plugin` >= 1.22
+Requires `mailer-plugin` >= 1.24
+
+## sample configuration
+
+```yaml
 unclassified:
   mailer:
-    adminAddress: admin@acme.org
+    # adminAddress: admin@acme.org          # This is deprecated
     replyToAddress: do-not-reply@acme.org
-    #smtpHost: smtp.acme.org
+    smtpHost: smtp.acme.org
     smtpPort: 4441
+```
--- integrations/src/test/java/io/jenkins/plugins/casc/MailerTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/MailerTest.java
@@ -2,7 +2,7 @@ package io.jenkins.plugins.casc;
 
 import hudson.tasks.Mailer;
 import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
-import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
 import jenkins.model.Jenkins;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,19 +15,19 @@ import static org.junit.Assert.assertEquals;
 public class MailerTest {
 
     @Rule
-    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+    public JenkinsConfiguredWithReadmeRule j = new JenkinsConfiguredWithReadmeRule();
 
     @Test
-    @ConfiguredWithReadme("demos/mailer/README.md")
+    @ConfiguredWithReadme("mailer/README.md")
     public void configure_mailer() throws Exception {
         final Jenkins jenkins = Jenkins.get();
         final Mailer.DescriptorImpl descriptor = (Mailer.DescriptorImpl) jenkins.getDescriptor(Mailer.class);
         assertEquals("4441", descriptor.getSmtpPort());
         assertEquals("do-not-reply@acme.org", descriptor.getReplyToAddress());
-        // assertEquals("smtp.acme.org", descriptor.getSmtpServer() /* getSmtpHost */ );
+        assertEquals("smtp.acme.org", descriptor.getSmtpHost() );
 
         // FIXME setAdminAddress is deprecated and should NOT be set this way
         // see https://github.com/jenkinsci/mailer-plugin/pull/39
-        assertEquals("admin@acme.org", descriptor.getAdminAddress());
+        //assertEquals("admin@acme.org", descriptor.getAdminAddress());
     }
 }
