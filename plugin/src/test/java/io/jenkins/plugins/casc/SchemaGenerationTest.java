046acd90b4b690f049d956a29597aca9132bdc40
Sladyn <gunnerforlife00@gmail.com>
2019-09-28 10:56:04 +0530

    Assert True functionality added

--- plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.casc;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.misc.Util;
 import java.util.logging.Logger;
+import org.dom4j.datatype.InvalidSchemaException;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
@@ -11,55 +12,55 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static io.jenkins.plugins.casc.SchemaGeneration.generateSchema;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SchemaGenerationTest {
 
-    public static final Logger LOGGER = Logger.getLogger(TokenReloadAction.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(TokenReloadAction.class.getName());
     private final String failureMessage  = "The YAML file provided for this schema is invalid";
 
     @Rule
     public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
     @Test
-    public void validSchemaShouldSucceed() throws Exception {
+    public void validSchemaShouldSucceed() {
         Schema schema = returnSchema();
-        try {
-            validateSchema(schema, returnJSONSubject("validSchemaConfig.yml"));
-        } catch (Exception e) {
-            LOGGER.warning(failureMessage);
-            fail(e.getMessage());
-        }
+        assertTrue(validateSchema(schema, returnJSONSubject("validSchemaConfig.yml")));
     }
 
     @Test
-    public void invalidSchemaShouldNotSucceed() throws Exception {
+    public void invalidSchemaShouldNotSucceed() {
         Schema schema = returnSchema();
-        try {
-            validateSchema(schema, returnJSONSubject("invalidSchemaConfig.yml"));
-            LOGGER.warning(failureMessage);
-            fail();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        assertFalse(validateSchema(schema, returnJSONSubject("invalidSchemaConfig.yml")));
+
     }
 
     private Schema returnSchema() {
         JSONObject schemaObject = generateSchema();
         JSONObject jsonSchema = new JSONObject(
             new JSONTokener(schemaObject.toString()));
-        Schema schema = SchemaLoader.load(jsonSchema);
-        return schema;
+        return SchemaLoader.load(jsonSchema);
     }
 
-    private JSONObject returnJSONSubject(String yamlFile) throws Exception {
-        String yamlStringContents = Util.toStringFromYamlFile(this, yamlFile);
-        JSONObject jsonSubject = new JSONObject(
+    private JSONObject returnJSONSubject(String yamlFile)  {
+        String yamlStringContents = null;
+        try {
+            yamlStringContents = Util.toStringFromYamlFile(this, yamlFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new JSONObject(
             new JSONTokener(Util.convertToJson(yamlStringContents)));
-        return jsonSubject;
     }
 
-    private void validateSchema(Schema schema, JSONObject jsonSubject) throws Exception {
-        schema.validate(jsonSubject);
+    private boolean validateSchema(Schema schema, JSONObject jsonSubject) {
+        try {
+            schema.validate(jsonSubject);
+        } catch (InvalidSchemaException ie) {
+            LOGGER.warning(failureMessage + ie);
+            return false;
+        }
+        return true;
     }
 }
