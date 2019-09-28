c41bdfb257441b76bf9458d166a649f5e1e933a6
Sladyn <gunnerforlife00@gmail.com>
2019-09-28 11:22:45 +0530

    Checkstyle warnings fix

--- plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
+++ plugin/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SchemaGenerationTest {
-    
+
     @Rule
     public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
