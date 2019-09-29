706e883bf09b7a94ce76cacd52b927c4034bc6f2
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-29 21:59:06 +0100

    Validate github demo with the integrations framework

---
+++ integrations/src/test/java/io/jenkins/plugins/casc/GitHubTest.java
@@ -0,0 +1,38 @@
+package io.jenkins.plugins.casc;
+
+import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
+import jenkins.model.GlobalConfiguration;
+import org.jenkinsci.plugins.github.config.GitHubPluginConfig;
+import org.jenkinsci.plugins.github.config.GitHubServerConfig;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author v1v (Victor Martinez)
+ */
+public class GitHubTest {
+
+    @Rule
+    public JenkinsConfiguredWithReadmeRule j = new JenkinsConfiguredWithReadmeRule();
+
+    @Test
+    @ConfiguredWithReadme("github/README.md")
+    public void configure_github() throws Exception {
+
+        final GitHubPluginConfig configuration = GlobalConfiguration.all().get(GitHubPluginConfig.class);
+        assertThat(configuration.getConfigs(), hasSize(1));
+
+        GitHubServerConfig config = configuration.getConfigs().get(0);
+        assertThat(config.getApiUrl(), is("https://github.domain.local/api/v3"));
+        assertThat(config.getCredentialsId(), is("[GitHubEEUser]"));
+        assertThat(config.getName(), is("InHouse GitHub EE"));
+        assertTrue(config.isManageHooks());
+
+    }
+}
