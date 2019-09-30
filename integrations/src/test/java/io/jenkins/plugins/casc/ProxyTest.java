867bbce29276b48fab27d513da01b0e0fb700516
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-09-30 21:15:53 +0100

    Validate proxy demo with the integrations framework

---
+++ integrations/src/test/java/io/jenkins/plugins/casc/ProxyTest.java
@@ -0,0 +1,34 @@
+package io.jenkins.plugins.casc;
+
+import hudson.ProxyConfiguration;
+import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
+import jenkins.model.Jenkins;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author v1v (Victor Martinez)
+ */
+public class ProxyTest {
+
+    @Rule
+    public JenkinsConfiguredWithReadmeRule j = new JenkinsConfiguredWithReadmeRule();
+
+    @Test
+    @ConfiguredWithReadme("proxy/README.md")
+    public void configure_proxy() throws Exception {
+        final ProxyConfiguration proxy = Jenkins.get().proxy;
+        assertNotNull(proxy);
+        assertThat(proxy.getPassword(), is("password"));
+        assertThat(proxy.getTestUrl(), is("http://google.com"));
+        assertThat(proxy.getUserName(), is("login"));
+        assertThat(proxy.name, is("proxyhost"));
+        assertThat(proxy.noProxyHost, is("externalhost"));
+        assertThat(proxy.port, is(80));
+    }
+}
