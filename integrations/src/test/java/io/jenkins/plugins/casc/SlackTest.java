6093f555d5a888a1eb2296fa12c9f728706d2d30
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-10-01 22:10:13 +0100

    fix the linting of adding at least one assert

--- integrations/src/test/java/io/jenkins/plugins/casc/SlackTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/SlackTest.java
@@ -1,11 +1,15 @@
 package io.jenkins.plugins.casc;
 
+import hudson.ExtensionList;
 import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
+import jenkins.plugins.slack.SlackNotifier;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.junit.rules.RuleChain;
+
+import static junit.framework.TestCase.assertNotNull;
 
 /**
  * @author v1v (Victor Martinez)
@@ -20,7 +24,9 @@ public class SlackTest {
     @Test
     @ConfiguredWithReadme("slack/README.md")
     public void configure_slack() throws Exception {
-        // Already validated within the plugin itself
+        // Already validated within the plugin itself, so let's run some simple validations
         // https://github.com/jenkinsci/slack-plugin/pull/582
+        SlackNotifier.DescriptorImpl slackNotifier = ExtensionList.lookupSingleton(SlackNotifier.DescriptorImpl.class);
+        assertNotNull(slackNotifier);
     }
 }
