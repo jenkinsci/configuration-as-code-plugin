950f9c841d7b1b69ead28ce49edebccc098d4ca9
Joseph Petersen <josephp90@gmail.com>
2020-01-11 02:34:09 +0100

    fix report aggregation part 3

---
+++ report/src/main/java/io/jenkins/plugins/casc/Dummy.java
@@ -0,0 +1,14 @@
+package io.jenkins.plugins.casc;
+
+public class Dummy {
+
+    private String hello;
+
+    public Dummy(String hello) {
+        this.hello = hello;
+    }
+
+    public String getHello() {
+        return hello;
+    }
+}
--- report/src/test/java/io/jenkins/plugins/casc/ReportTest.java
+++ report/src/test/java/io/jenkins/plugins/casc/ReportTest.java
@@ -8,6 +8,9 @@ public class ReportTest {
     @Test
     public void test() {
         // dummy test to ensure jacoco data execution
+        String actual = "world";
+        Dummy world = new Dummy(actual);
+        Assert.assertEquals(world.getHello(), actual);
         Assert.assertTrue(true);
     }
 
