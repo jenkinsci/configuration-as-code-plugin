3190440a7e4adde7e20d98edb37f4cfa7c5483e7
Joseph Petersen <josephp90@gmail.com>
2020-01-11 01:43:23 +0100

    fix report aggregation part 2

---
+++ report/src/test/java/io/jenkins/plugins/casc/ReportTest.java
@@ -0,0 +1,14 @@
+package io.jenkins.plugins.casc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReportTest {
+
+    @Test
+    public void test() {
+        // dummy test to ensure jacoco data execution
+        Assert.assertTrue(true);
+    }
+
+}
