475905c8d0323bac840b749a0ef7beb5646aa3c6
Tim Jacomb <timjacomb1+github@gmail.com>
2020-02-20 21:00:50 +0000

    Fix checkstyle

--- test-harness/src/test/java/io/jenkins/plugins/casc/JenkinsConfiguredWithCodeRuleClassRuleTest.java
+++ test-harness/src/test/java/io/jenkins/plugins/casc/JenkinsConfiguredWithCodeRuleClassRuleTest.java
@@ -1,17 +1,12 @@
 package io.jenkins.plugins.casc;
 
 import hudson.model.User;
-import hudson.security.ACL;
-import hudson.security.HudsonPrivateSecurityRealm;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
-import io.jenkins.plugins.casc.model.CNode;
-import io.jenkins.plugins.casc.model.Mapping;
 import java.util.Collections;
 import org.junit.ClassRule;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JenkinsConfiguredWithCodeRuleClassRuleTest {
