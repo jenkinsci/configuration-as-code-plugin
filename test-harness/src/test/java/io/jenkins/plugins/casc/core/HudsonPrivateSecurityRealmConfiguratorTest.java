560ac2bf1edba601dacb8b41f0a0770d99e0a3af
Tim Jacomb <tim.jacomb@hmcts.net>
2020-02-20 21:23:01 +0000

    Update HudsonPrivateSecurityRealmConfiguratorTest.java

--- test-harness/src/test/java/io/jenkins/plugins/casc/core/HudsonPrivateSecurityRealmConfiguratorTest.java
+++ test-harness/src/test/java/io/jenkins/plugins/casc/core/HudsonPrivateSecurityRealmConfiguratorTest.java
@@ -10,7 +10,6 @@ import org.junit.Assume;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
