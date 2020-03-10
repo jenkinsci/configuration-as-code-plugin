c04d963bbf742ed2090eaaf2dbb19365f73a7bd7
Tim Jacomb <timjacomb1+github@gmail.com>
2020-03-10 11:00:40 +0000

    Support for additional user attributes (#1309)
    Co-authored-by: Joseph Petersen <josephp90@gmail.com>

--- plugin/src/main/java/io/jenkins/plugins/casc/core/HudsonPrivateSecurityRealmConfigurator.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/core/HudsonPrivateSecurityRealmConfigurator.java
@@ -3,6 +3,8 @@ package io.jenkins.plugins.casc.core;
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
+import hudson.model.User;
+import hudson.model.UserProperty;
 import hudson.security.HudsonPrivateSecurityRealm;
 import io.jenkins.plugins.casc.Attribute;
 import io.jenkins.plugins.casc.ConfigurationContext;
@@ -11,6 +13,7 @@ import io.jenkins.plugins.casc.impl.configurators.DataBoundConfigurator;
 import io.jenkins.plugins.casc.model.CNode;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,6 +22,7 @@ import org.apache.commons.lang.StringUtils;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 /**
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
@@ -47,35 +51,75 @@ public class HudsonPrivateSecurityRealmConfigurator extends DataBoundConfigurato
 
     @CheckForNull
     @Override
-    public CNode describe(HudsonPrivateSecurityRealm instance, ConfigurationContext context) {
+    public CNode describe(HudsonPrivateSecurityRealm instance, ConfigurationContext context)
+        throws Exception {
+        // allow disabling exporting users if an instance has too many
+        if (System.getProperty("io.jenkins.plugins.casc.core.HudsonPrivateSecurityRealmConfigurator.exportUsers", "true").equals("true")) {
+            return super.describe(instance, context);
+        }
         return null;
     }
 
     private static Collection<UserWithPassword> getter(HudsonPrivateSecurityRealm target) {
         return target.getAllUsers().stream()
-                .map(u -> new UserWithPassword(u.getId(), null)) // password isn't actually stored, only hashed
+                .map(u -> {
+                    UserWithPassword user = new UserWithPassword(u.getId(), null);
+                    user.setName(u.getFullName());
+                    user.setDescription(u.getDescription());
+                    List<UserProperty> properties = u.getAllProperties()
+                        .stream()
+                        .filter(userProperty -> !userProperty.getClass().getName().equals("com.cloudbees.plugins.credentials.UserCredentialsProvider$UserCredentialsProperty"))
+                        .collect(Collectors.toList());
+                    user.setProperties(properties);
+
+                    return user;
+                })
                 .collect(Collectors.toList());
     }
 
     private static void setter(HudsonPrivateSecurityRealm target, Collection<UserWithPassword> value) throws IOException {
         for (UserWithPassword user : value) {
-            if (StringUtils.startsWith(user.password, HASHED_PASSWORD_PREFIX)) {
-                try {
-                    target.createAccountWithHashedPassword(user.id, user.password);
-                } catch (IllegalArgumentException e) {
-                    logger.log(Level.WARNING, "Failed to create user with presumed hashed password", e);
-                    // fallback, just create the account as is
-                    target.createAccount(user.id, user.password);
+            User updatedUser = createAccount(target, user);
+            updatedUser.setFullName(user.name);
+            updatedUser.setDescription(user.description);
+            if (user.getProperties() != null) {
+                for (UserProperty property : user.getProperties()) {
+                    updatedUser.addProperty(property);
                 }
-            } else {
-                target.createAccount(user.id, user.password);
             }
         }
+    }
+
+    private static User createAccount(HudsonPrivateSecurityRealm target, UserWithPassword user)
+        throws IOException {
+        User updatedUser;
+        if (StringUtils.isNotBlank(user.password)) {
+            if (StringUtils.startsWith(user.password, HASHED_PASSWORD_PREFIX)) {
+                try {
+                    updatedUser = target
+                        .createAccountWithHashedPassword(user.id, user.password);
+                } catch (IllegalArgumentException | IOException e) {
+                    logger.log(Level.WARNING,
+                        "Failed to create user with presumed hashed password", e);
+                    // fallback, just create the account as is
+                    updatedUser = target.createAccount(user.id, user.password);
+                }
+            } else {
+                updatedUser = target.createAccount(user.id, user.password);
+            }
+        } else {
+            updatedUser = User.getById(user.id, true);
+        }
+        return updatedUser;
     }
 
     public static class UserWithPassword {
         private final String id;
         private final String password;
+
+        private String name;
+        private String description;
+        private List<UserProperty> properties;
 
         @DataBoundConstructor
         public UserWithPassword(String id, String password) {
@@ -83,5 +127,35 @@ public class HudsonPrivateSecurityRealmConfigurator extends DataBoundConfigurato
             this.password = password;
         }
 
+        @DataBoundSetter
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @DataBoundSetter
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        @DataBoundSetter
+        public void setProperties(List<UserProperty> properties) {
+            this.properties = properties;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public List<UserProperty> getProperties() {
+            return properties;
+        }
     }
 }
