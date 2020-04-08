5dc6d9f53ccb8f76d399117ca052b289d1b70a07
Joseph Petersen <josephp90@gmail.com>
2020-04-09 00:24:46 +0200

    update test

--- integrations/src/test/java/io/jenkins/plugins/casc/CheckmarxTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/CheckmarxTest.java
@@ -1,26 +1,31 @@
 package io.jenkins.plugins.casc;
 
 import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
 import jenkins.model.Jenkins;
 
 import com.checkmarx.jenkins.CxScanBuilder;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class CheckmarxTest {
 
+    @Rule
+    public JenkinsConfiguredWithReadmeRule j = new JenkinsConfiguredWithReadmeRule();
+
     @Test
-    @ConfiguredWithReadme("checkmarx/README.md")
+    @ConfiguredWithReadme(value = "checkmarx/README.md")
     public void configure_checkmarx() throws Exception {
         final Jenkins jenkins = Jenkins.get();
         final CxScanBuilder.DescriptorImpl cxScanConfig = (CxScanBuilder.DescriptorImpl) jenkins.getDescriptor(CxScanBuilder.class);
-        assertTrue(descriptor.getUseCredentialsPlugin());
-
+        assertThat(cxScanConfig, is(notNullValue()));
+        assertThat(cxScanConfig.isOldCredentials(), is(false));
         assertThat(cxScanConfig.getServerUrl(), is(equalTo("https://checkmarx.url.tld")));
     }
 }
