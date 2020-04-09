435694623a81615d315e433ec5df46259a2da50b
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2020-04-09 19:03:38 +0100

    Improve SecretSource Javadoc (#1302)
    Co-authored-by: Joseph Petersen <josephp90@gmail.com>

--- plugin/src/main/java/io/jenkins/plugins/casc/Configurator.java
+++ plugin/src/main/java/io/jenkins/plugins/casc/Configurator.java
@@ -115,7 +115,7 @@ public interface Configurator<T> {
 
 
     /**
-     * @return list of {@link Configurator<T>}s to be considered so one can fully configure this component.
+     * @return list of {@link Configurator}s to be considered so one can fully configure this component.
      * Typically, configurator for an abstract extension point will return Configurators for available implementations.
      */
     @NonNull
