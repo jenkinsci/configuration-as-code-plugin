c19e7a5f88a36ab1d778f78a45d58d45559e6164
Joseph Petersen <josephp90@gmail.com>
2020-06-14 20:57:33 +0200

    imports

--- integrations/src/test/java/io/jenkins/plugins/casc/Crowd2Test.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/Crowd2Test.java
@@ -1,23 +1,24 @@
 package io.jenkins.plugins.casc;
 
+import de.theit.jenkins.crowd.CrowdSecurityRealm;
 import hudson.security.SecurityRealm;
 import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
-import de.theit.jenkins.crowd.CrowdSecurityRealm;
 import jenkins.model.Jenkins;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.junit.rules.RuleChain;
 
-import static org.jvnet.hudson.test.JenkinsMatchers.hasPlainText;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.jvnet.hudson.test.JenkinsMatchers.hasPlainText;
 
 public class Crowd2Test {
 
     public static final String PASSWORD_123 = "password123";
+
     @Rule
     public RuleChain chain = RuleChain.outerRule(new EnvironmentVariables()
         .set("CROWD_PASSWORD", PASSWORD_123))
@@ -28,7 +29,7 @@ public class Crowd2Test {
     public void configure_artifact_manager() throws Exception {
         SecurityRealm realm = Jenkins.get().getSecurityRealm();
         assertThat(realm, instanceOf(CrowdSecurityRealm.class));
-        CrowdSecurityRealm securityRealm = (CrowdSecurityRealm)realm;
+        CrowdSecurityRealm securityRealm = (CrowdSecurityRealm) realm;
         assertThat(securityRealm.applicationName, is("jenkins"));
         assertThat(securityRealm.group, is("jenkins-users"));
         assertThat(securityRealm.url, is("http://crowd.company.io"));
