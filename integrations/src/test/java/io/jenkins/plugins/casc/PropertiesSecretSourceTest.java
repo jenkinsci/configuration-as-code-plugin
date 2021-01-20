859aa51974fc2388cdd35c539a1a9ee391e754aa
Aliaksei Savanchuk <dntsaygoodbye@gmail.com>
2021-01-20 21:49:13 +0300

    Add test to check that secrets are realoded when we change properties file and do reload JCasC

--- integrations/src/test/java/io/jenkins/plugins/casc/PropertiesSecretSourceTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/PropertiesSecretSourceTest.java
@@ -4,8 +4,13 @@ import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import jenkins.model.Jenkins;
 import org.junit.Rule;
 import org.junit.Test;
@@ -34,5 +39,37 @@ public class PropertiesSecretSourceTest {
         // https://leahneukirchen.org/blog/archive/2019/10/ken-thompson-s-unix-password.html
         assertEquals("ken", credentials.getUsername());
         assertEquals("p/q2-q4!", credentials.getPassword().getPlainText());
+    }
+
+    @Test
+    @ConfiguredWithCode("PropertiesSecretSourceTest.yaml")
+    public void testSecretsFromPropertiesAreUpdatedAfterReload() throws Exception {
+        File secretsFile =  new File(getClass().getResource("secrets.properties").getFile());
+        Properties secrets = new Properties();
+        InputStream inputStream = new FileInputStream(secretsFile);
+        secrets.load(inputStream);
+
+        FileWriter fileWriter = new FileWriter(secretsFile);
+
+        String secretName = "testuser";
+        String updatedTestUserSecret = "charmander";
+        secrets.setProperty(secretName, updatedTestUserSecret);
+        try {
+            secrets.store(fileWriter, "store to properties file");
+
+            ConfigurationAsCode.get().configure(this.getClass().getResource("PropertiesSecretSourceTest.yaml").toString());
+
+            List<UsernamePasswordCredentials> credentialList = CredentialsProvider
+                .lookupCredentials(UsernamePasswordCredentials.class,
+                    Jenkins.getInstanceOrNull(), null, Collections.emptyList());
+            assertEquals(1, credentialList.size());
+
+            UsernamePasswordCredentials credentials = credentialList.get(0);
+
+            assertEquals(updatedTestUserSecret, credentials.getUsername());
+        } finally {
+            secrets.setProperty(secretName, "ken");
+            secrets.store(fileWriter, "store to properties file");
+        }
     }
 }
