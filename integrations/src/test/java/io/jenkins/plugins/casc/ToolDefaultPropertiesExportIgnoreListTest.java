b6ce3244c69235867333aa970447c25c601e1978
Mark Waite <mark.earl.waite@gmail.com>
2021-03-10 14:15:57 -0700

    Integration test with baseline of next LTS (#1577)
    Co-authored-by: Tim Jacomb <timjacomb1+github@gmail.com>

--- integrations/src/test/java/io/jenkins/plugins/casc/ToolDefaultPropertiesExportIgnoreListTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/ToolDefaultPropertiesExportIgnoreListTest.java
@@ -3,7 +3,7 @@ package io.jenkins.plugins.casc;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.model.CNode;
-import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 
@@ -14,14 +14,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 
-public class ToolDefaultPropertiesExportBlacklistTest {
+public class ToolDefaultPropertiesExportIgnoreListTest {
 
-    @ClassRule
-    @ConfiguredWithCode("ToolDefaultPropertiesExportBlacklist.yml")
-    public static JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
+    @Rule
+    public JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
     @Test
     @Issue("JENKINS-57122")
+    @ConfiguredWithCode("ToolDefaultPropertiesExportIgnoreList.yml")
     public void export_tool_configuration() throws Exception {
         ConfiguratorRegistry registry = ConfiguratorRegistry.get();
         ConfigurationContext context = new ConfigurationContext(registry);
@@ -29,7 +29,7 @@ public class ToolDefaultPropertiesExportBlacklistTest {
 
         String exported = toYamlString(yourAttribute).replaceAll("git\\.exe", "git");
 
-        String expected = toStringFromYamlFile(this, "ToolDefaultPropertiesExportBlacklistExpected.yml");
+        String expected = toStringFromYamlFile(this, "ToolDefaultPropertiesExportIgnoreListExpected.yml");
 
         assertThat(exported, is(expected));
     }
