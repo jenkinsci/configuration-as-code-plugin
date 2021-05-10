f970fecf62104130590e366dfbb97c914f5dbb7c
dheerajodha <dheerajsinghjodha@yahoo.com.au>
2021-05-10 19:34:52 +0530

    Adding Test for View-Job-Filters plugin

---
+++ demos/view-job-filters/README.md
@@ -0,0 +1,42 @@
+# Configure View Job Filters
+
+Basic configuration of the [View Job Filters plugin](https://plugins.jenkins.io/view-job-filters/)
+
+## Sample configuration
+
+```yaml
+jenkins:
+  views:
+    - list:
+        columns:
+        - "status"
+        - "weather"
+        - "jobName"
+        - "lastSuccess"
+        - "lastFailure"
+        - "lastDuration"
+        - "buildButton"
+        jobFilters:
+        - buildDurationFilter:
+            amount: "60.0"
+            amountTypeString: "Days"
+            buildCountTypeString: "Latest"
+            buildDurationMinutes: "5"
+            includeExcludeTypeString: "includeMatched"
+            lessThan: true
+        - buildStatusFilter:
+            building: false
+            inBuildQueue: true
+            includeExcludeTypeString: "includeMatched"
+            neverBuilt: true
+        - jobTypeFilter:
+            includeExcludeTypeString: "includeMatched"
+            jobType: "hudson.model.FreeStyleProject$DescriptorImpl@5d0507bc"
+        - securityFilter:
+            build: false
+            configure: true
+            includeExcludeTypeString: "includeMatched"
+            permissionCheckType: "MustMatchAll"
+            workspace: false
+        name: "MyFirstView"
+```
---
+++ integrations/src/test/java/io/jenkins/plugins/casc/ViewJobFiltersTest.java
@@ -0,0 +1,60 @@
+package io.jenkins.plugins.casc;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import java.util.List;
+
+import hudson.views.BuildDurationFilter;
+import hudson.views.BuildStatusFilter;
+import hudson.views.JobTypeFilter;
+import hudson.views.SecurityFilter;
+
+import org.jfrog.hudson.ArtifactoryBuilder;
+import org.jfrog.hudson.ArtifactoryServer;
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.jenkins.plugins.casc.misc.ConfiguredWithReadme;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithReadmeRule;
+import jenkins.model.Jenkins;
+
+public class ViewJobFiltersTest {
+	
+	@Rule
+    public JenkinsConfiguredWithReadmeRule j = new JenkinsConfiguredWithReadmeRule();
+	
+	@Test
+    @ConfiguredWithReadme(value = "view-job-filters/README.md")
+    public void configure_view_job_filters() throws Exception {
+		
+		final BuildDurationFilter buildDurationFilter = new BuilDurationFilter();
+		assertTrue(buildDurationFilter.isLessThan());
+		assertThat(buildDurationFilter.getBuildDurationMinutes, is(5));
+		assertThat(buildDurationFilter.getAmount(), is(60.0));
+		assertThat(buildDurationFilter.getAmountTypeString(), is("Days"));
+		assertThat(buildDurationFilter.getBuildCountTypeString(), is("Latest"));
+		assertTrue(buildDurationFilter.isIncludeMatched());	
+		
+		final BuildStatusFilter buildStatusFilter = new BuildStatusFilter();
+		assertTrue(buildStatusFilter.isNeverBuilt());
+		assertFalse(buildStatusFilter.isBuilding());
+		assertTrue(buildStatusFilter.isInBuildQueue());
+		assertTrue(buildStatusFilter.isIncludeMatched());
+		
+		final JobTypeFilter jobTypeFilter = new JobTypeFilter();
+		assertThat(jobTypeFilter.getJobType(), is("hudson.model.FreeStyleProject$DescriptorImpl@5d0507bc"));
+		assertTrue(buildStatusFilter.isIncludeMatched());
+		
+		final SecurityFilter securityFilter = new SecurityFilter();
+		assertThat(securityFilter.getPermissionCheckType(), is("MustMatchAll"));
+		assertTrue(securityFilter.isConfigure());
+		assertFalse(securityFilter.isBuild());
+		assertFalse(securityFilter.isWorkspace());
+		assertTrue(buildStatusFilter.isIncludeMatched());
+    }
+}
