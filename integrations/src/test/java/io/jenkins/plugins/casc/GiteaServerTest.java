f4f3e68946b752b9f845ec5fb56b3fd9edc5dd93
justusbunsi <61625851+justusbunsi@users.noreply.github.com>
2021-11-27 16:23:35 +0100

    Rename existing server configuration test

--- integrations/src/test/java/io/jenkins/plugins/casc/GiteaServerTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/GiteaServerTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class GiteaTest {
+public class GiteaServerTest {
 
     @Rule
     public JenkinsConfiguredWithReadmeRule j = new JenkinsConfiguredWithReadmeRule();
