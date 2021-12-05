b47d1c360a335b73eb0fec9d0abc225228066156
justusbunsi <61625851+justusbunsi@users.noreply.github.com>
2021-12-05 17:35:51 +0100

    Fix + extend Gitea organization folder test
    Requires `branch-api` plugin to be loaded. All the other plugins added
    along with it are dependencies.
    
    Signed-off-by: Steven Kriegler <61625851+justusbunsi@users.noreply.github.com>

--- integrations/src/test/java/io/jenkins/plugins/casc/GiteaOrganisationFolderTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/GiteaOrganisationFolderTest.java
@@ -2,28 +2,68 @@ package io.jenkins.plugins.casc;
 
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import java.util.List;
 import jenkins.branch.OrganizationFolder;
-import jenkins.model.Jenkins;
+import jenkins.scm.api.trait.SCMTrait;
+import org.jenkinsci.plugin.gitea.BranchDiscoveryTrait;
+import org.jenkinsci.plugin.gitea.ExcludeArchivedRepositoriesTrait;
+import org.jenkinsci.plugin.gitea.ForkPullRequestDiscoveryTrait;
+import org.jenkinsci.plugin.gitea.GiteaSCMNavigator;
+import org.jenkinsci.plugin.gitea.OriginPullRequestDiscoveryTrait;
+import org.jenkinsci.plugin.gitea.SSHCheckoutTrait;
+import org.jenkinsci.plugin.gitea.TagDiscoveryTrait;
+import org.jenkinsci.plugin.gitea.WebhookRegistrationTrait;
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.WithPlugin;
 
-// import static org.hamcrest.MatcherAssert.assertThat;
-// import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-// import static org.junit.Assert.assertTrue;
 
 public class GiteaOrganisationFolderTest {
 
     @Rule
-    public JenkinsRule j = new JenkinsConfiguredWithCodeRule();
+    public JenkinsConfiguredWithCodeRule r = new JenkinsConfiguredWithCodeRule();
 
     @Test
+    @WithPlugin({"branch-api.jpi", "cloudbees-folder.jpi", "scm-api.jpi", "structs.jpi"})
     @ConfiguredWithCode("SeedJobTest_withGiteaOrganisation.yml")
     public void configure_gitea_organisation_folder_seed_job() {
-        final Jenkins jenkins = Jenkins.get();
-
-        OrganizationFolder folder = (OrganizationFolder) jenkins.getItem("Gitea Organization Folder");
+        OrganizationFolder folder = (OrganizationFolder) r.jenkins.getItem("Gitea Organization Folder");
         assertNotNull(folder);
+
+        GiteaSCMNavigator organization = (GiteaSCMNavigator) folder.getNavigators().get(0);
+        assertNotNull(organization);
+
+        assertEquals("gitea-token", organization.getCredentialsId());
+        assertEquals("https://git.example.com", organization.getServerUrl());
+        assertEquals("OWN", organization.getRepoOwner());
+
+        List<SCMTrait<?>> traits = organization.getTraits();
+        assertEquals(7, traits.size());
+
+        assertEquals(ExcludeArchivedRepositoriesTrait.class, traits.get(0).getClass());
+        assertEquals(TagDiscoveryTrait.class, traits.get(1).getClass());
+
+        SCMTrait<?> trait2 = traits.get(2);
+        assertEquals(BranchDiscoveryTrait.class, trait2.getClass());
+        assertEquals(1, ((BranchDiscoveryTrait)trait2).getStrategyId());
+
+        SCMTrait<?> trait3 = traits.get(3);
+        assertEquals(OriginPullRequestDiscoveryTrait.class, trait3.getClass());
+        assertEquals(2, ((OriginPullRequestDiscoveryTrait)trait3).getStrategyId());
+
+        SCMTrait<?> trait4 = traits.get(4);
+        assertEquals(ForkPullRequestDiscoveryTrait.class, trait4.getClass());
+        assertEquals(1, ((ForkPullRequestDiscoveryTrait)trait4).getStrategyId());
+        assertEquals(ForkPullRequestDiscoveryTrait.TrustContributors.class, ((ForkPullRequestDiscoveryTrait)trait4).getTrust().getClass());
+
+        SCMTrait<?> trait5 = traits.get(5);
+        assertEquals(WebhookRegistrationTrait.class, trait5.getClass());
+        assertEquals("ITEM", ((WebhookRegistrationTrait)trait5).getMode().name());
+
+        SCMTrait<?> trait6 = traits.get(6);
+        assertEquals(SSHCheckoutTrait.class, trait6.getClass());
+        assertEquals("ssh-gitea", ((SSHCheckoutTrait)trait6).getCredentialsId());
     }
 }
