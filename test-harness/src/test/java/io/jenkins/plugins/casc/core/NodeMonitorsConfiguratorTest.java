f8cea496d59d399618ed70568eb4f26476195d0f
Markus Winter <m.winter@sap.com>
2023-11-23 17:26:29 +0100

    more tests

--- test-harness/src/test/java/io/jenkins/plugins/casc/core/NodeMonitorsConfiguratorTest.java
+++ test-harness/src/test/java/io/jenkins/plugins/casc/core/NodeMonitorsConfiguratorTest.java
@@ -4,10 +4,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import hudson.model.ComputerSet;
+import hudson.node_monitors.ArchitectureMonitor;
 import hudson.node_monitors.DiskSpaceMonitor;
 import hudson.node_monitors.ResponseTimeMonitor;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import jenkins.model.Jenkins;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -21,12 +23,17 @@ public class NodeMonitorsConfiguratorTest {
     public void should_configure_node_monitors() {
         DiskSpaceMonitor dsm = (DiskSpaceMonitor) ComputerSet.getMonitors().get(DiskSpaceMonitor.DESCRIPTOR);
         assertThat(dsm.freeSpaceThreshold, is("3GB"));
+        ArchitectureMonitor.DescriptorImpl amd =
+                (ArchitectureMonitor.DescriptorImpl) Jenkins.get().getDescriptorOrDie(ArchitectureMonitor.class);
+        ArchitectureMonitor am = (ArchitectureMonitor) ComputerSet.getMonitors().get(amd);
+        assertThat(am.isIgnored(), is(false));
     }
 
     @Test
     @ConfiguredWithCode("NodeMonitors.yml")
     public void not_configured_monitors_are_ignored() {
-        ResponseTimeMonitor rtm = (ResponseTimeMonitor) ComputerSet.getMonitors().get(ResponseTimeMonitor.DESCRIPTOR);
+        ResponseTimeMonitor rtm =
+                (ResponseTimeMonitor) ComputerSet.getMonitors().get(ResponseTimeMonitor.DESCRIPTOR);
         assertThat(rtm.isIgnored(), is(true));
     }
 }
