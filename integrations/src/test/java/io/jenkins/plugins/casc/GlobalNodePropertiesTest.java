148f87d74b1e1623babf4b4b3376c879acff35a4
Tim Jacomb <21194782+timja@users.noreply.github.com>
2025-04-07 10:18:46 +0100

    Revert "Allowing empty valued global variables in config file (#2657)" (#2663)
    This reverts commit 506f96a69516cae7f8826bb7e27ae99e825b43e0.

--- integrations/src/test/java/io/jenkins/plugins/casc/GlobalNodePropertiesTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/GlobalNodePropertiesTest.java
@@ -7,19 +7,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import hudson.node_monitors.DiskSpaceMonitorNodeProperty;
 import hudson.slaves.EnvironmentVariablesNodeProperty;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.NodePropertyDescriptor;
-import hudson.tools.ToolLocationNodeProperty;
 import hudson.util.DescribableList;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.misc.junit.jupiter.WithJenkinsConfiguredWithCode;
 import io.jenkins.plugins.casc.model.CNode;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import jenkins.model.Jenkins;
 import org.junit.jupiter.api.Test;
@@ -34,37 +30,14 @@ class GlobalNodePropertiesTest {
 
         DescribableList<NodeProperty<?>, NodePropertyDescriptor> nodeProperties = jenkins.getGlobalNodeProperties();
 
-        assertEquals(3, nodeProperties.size());
-
-        Set<Map.Entry<String, String>> envVars = ((EnvironmentVariablesNodeProperty)
-                        nodeProperties.get(EnvironmentVariablesNodeProperty.class))
+        Set<Map.Entry<String, String>> entries = ((EnvironmentVariablesNodeProperty) nodeProperties.get(0))
                 .getEnvVars()
                 .entrySet();
-        assertEquals(3, envVars.size());
+        assertEquals(1, entries.size());
 
-        Iterator<Entry<String, String>> iterator = envVars.iterator();
-        Map.Entry<String, String> envVar = iterator.next();
+        Map.Entry<String, String> envVar = entries.iterator().next();
         assertEquals("FOO", envVar.getKey());
         assertEquals("BAR", envVar.getValue());
-
-        envVar = iterator.next();
-        assertEquals("FOO2", envVar.getKey());
-        assertEquals("", envVar.getValue());
-
-        envVar = iterator.next();
-        assertEquals("FOO3", envVar.getKey());
-        assertEquals("", envVar.getValue());
-
-        DiskSpaceMonitorNodeProperty diskSpace = nodeProperties.get(DiskSpaceMonitorNodeProperty.class);
-        assertEquals("1GiB", diskSpace.getFreeDiskSpaceThreshold());
-        assertEquals("2GiB", diskSpace.getFreeDiskSpaceWarningThreshold());
-        assertEquals("1GiB", diskSpace.getFreeTempSpaceThreshold());
-        assertEquals("2GiB", diskSpace.getFreeTempSpaceWarningThreshold());
-
-        ToolLocationNodeProperty toolLocations = nodeProperties.get(ToolLocationNodeProperty.class);
-        assertEquals(1, toolLocations.getLocations().size());
-        assertEquals("Default", toolLocations.getLocations().get(0).getName());
-        assertEquals("/home/user/bin/git", toolLocations.getLocations().get(0).getHome());
     }
 
     @Test
