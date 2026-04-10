ebc273b4f69326b1c5c37bd638d62613618ac223
somiljain2006 <somil16022006@gmail.com>
2026-04-10 12:50:41 +0530

    Add test coverage for certificate credentials in SystemCredentialsTest

--- integrations/src/test/java/io/jenkins/plugins/casc/SystemCredentialsTest.java
+++ integrations/src/test/java/io/jenkins/plugins/casc/SystemCredentialsTest.java
@@ -3,20 +3,30 @@ package io.jenkins.plugins.casc;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey.DirectEntryPrivateKeySource;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.CertificateCredentials;
 import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.impl.CertificateCredentialsImpl;
+import com.cloudbees.plugins.credentials.impl.CertificateCredentialsImpl.UploadedKeyStoreSource;
 import hudson.security.ACL;
 import io.jenkins.plugins.casc.impl.configurators.DataBoundConfigurator;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.model.CNode;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -41,7 +51,8 @@ public class SystemCredentialsTest {
     public RuleChain chain = RuleChain.outerRule(new EnvironmentVariables()
                     .set("SUDO_PASSWORD", "1234")
                     .set("SSH_PRIVATE_KEY", "s3cr3t")
-                    .set("SSH_KEY_PASSWORD", "ABCD"))
+                    .set("SSH_KEY_PASSWORD", "123456")
+                    .set("CERTIFICATE_BASE64", getBase64Keystore()))
             .around(log)
             .around(new JenkinsConfiguredWithCodeRule());
 
@@ -63,16 +74,35 @@ public class SystemCredentialsTest {
 
         List<CertificateCredentials> certs = CredentialsProvider.lookupCredentials(
                 CertificateCredentials.class, jenkins, ACL.SYSTEM, Collections.emptyList());
-        assertThat(certs, hasSize(0));
-        //       TODO: add test for uploaded certificate
-        //        assertThat(certs.get(0).getPassword().getPlainText(), equalTo("ABCD"));
+        assertThat(certs, hasSize(1));
+
+        CertificateCredentialsImpl certImpl = (CertificateCredentialsImpl) certs.get(0);
+        assertThat(certImpl.getId(), equalTo("uploaded_certificate"));
+        assertThat(certImpl.getPassword().getPlainText(), equalTo("123456"));
+        assertThat(certImpl.getKeyStoreSource(), notNullValue());
+        assertThat(certImpl.getKeyStoreSource(), instanceOf(UploadedKeyStoreSource.class));
+        assertThat(certImpl.getKeyStore(), notNullValue());
+        assertThat(certImpl.getKeyStore().size(), greaterThan(0));
+
+        UploadedKeyStoreSource keyStoreSource = (UploadedKeyStoreSource) certImpl.getKeyStoreSource();
+        assertThat(keyStoreSource.getUploadedKeystore(), notNullValue());
+
+        byte[] expectedBytes = getRawKeystoreBytes();
+        byte[] actualBytes = keyStoreSource.getUploadedKeystore().getPlainData();
+        assertThat("The bytes in Jenkins should be identical to the source file", actualBytes, equalTo(expectedBytes));
+
+        Enumeration<String> aliases = certImpl.getKeyStore().aliases();
+        assertThat("Keystore should not be empty", aliases.hasMoreElements(), equalTo(true));
+
+        String firstAlias = aliases.nextElement();
+        assertThat("Alias should contain a valid key", certImpl.getKeyStore().isKeyEntry(firstAlias), equalTo(true));
 
         List<BasicSSHUserPrivateKey> sshPrivateKeys = CredentialsProvider.lookupCredentials(
                 BasicSSHUserPrivateKey.class, jenkins, ACL.SYSTEM, Collections.emptyList());
         assertThat(sshPrivateKeys, hasSize(1));
 
         final BasicSSHUserPrivateKey ssh_with_passphrase = sshPrivateKeys.get(0);
-        assertThat(ssh_with_passphrase.getPassphrase().getPlainText(), equalTo("ABCD"));
+        assertThat(ssh_with_passphrase.getPassphrase().getPlainText(), equalTo("123456"));
 
         final DirectEntryPrivateKeySource source =
                 (DirectEntryPrivateKeySource) ssh_with_passphrase.getPrivateKeySource();
@@ -81,7 +111,23 @@ public class SystemCredentialsTest {
         // credentials should not appear in plain text in log
         for (LogRecord logRecord : log.getRecords()) {
             assertThat(logRecord.getMessage(), not(containsString("1234")));
-            assertThat(logRecord.getMessage(), not(containsString("ABCD")));
+            assertThat(logRecord.getMessage(), not(containsString("123456")));
+        }
+    }
+
+    private static String getBase64Keystore() {
+        return Base64.getEncoder().encodeToString(getRawKeystoreBytes());
+    }
+
+    private static byte[] getRawKeystoreBytes() {
+        try {
+            URL res = SystemCredentialsTest.class.getResource("test.p12");
+            if (res == null) {
+                throw new IllegalStateException("Cannot find test.p12 on classpath");
+            }
+            return Files.readAllBytes(Paths.get(res.toURI()));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read test.p12 from classpath", e);
         }
     }
 }
