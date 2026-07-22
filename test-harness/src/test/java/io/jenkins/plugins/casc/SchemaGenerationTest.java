a07be913ef93eaac0601b5b9dedd78bf813622ec
somiljain2006 <somil16022006@gmail.com>
2026-07-22 12:22:38 +0530

    Added tests

--- test-harness/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
+++ test-harness/src/test/java/io/jenkins/plugins/casc/SchemaGenerationTest.java
@@ -8,10 +8,17 @@ import static org.hamcrest.Matchers.empty;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import hudson.Extension;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
 import io.jenkins.plugins.casc.misc.junit.jupiter.WithJenkinsConfiguredWithCode;
+import java.util.List;
+import jenkins.model.GlobalConfiguration;
+import org.jenkinsci.Symbol;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 @WithJenkinsConfiguredWithCode
 @SuppressWarnings("unused")
@@ -79,6 +86,51 @@ class SchemaGenerationTest {
         JSONObject items = agentProtocols.getJSONObject("items");
         assertNotNull(items, "agentProtocols should have an 'items' definition");
         assertEquals("string", items.getString("type"), "agentProtocols items should be of type string");
+    }
+
+    @Test
+    void arrayEnumAttributesShouldGenerateAsEnumArrays(JenkinsConfiguredWithCodeRule j) {
+        JSONObject schema = SchemaGeneration.generateSchema();
+
+        JSONObject unclassifiedProps =
+                schema.getJSONObject("properties").getJSONObject("unclassified").getJSONObject("properties");
+        JSONObject dummyConfig = unclassifiedProps.getJSONObject("dummyConfig").getJSONObject("properties");
+        JSONObject myEnums = dummyConfig.getJSONObject("myEnums");
+
+        assertNotNull(myEnums, "myEnums should exist in the generated schema");
+        assertEquals("array", myEnums.getString("type"), "myEnums should be generated as an array type");
+
+        JSONObject items = myEnums.getJSONObject("items");
+        assertNotNull(items, "myEnums should have an 'items' definition");
+        assertEquals("string", items.getString("type"), "myEnums items should be of type string");
+
+        JSONArray enumValues = items.getJSONArray("enum");
+        assertEquals(2, enumValues.length());
+        assertEquals("VALUE_A", enumValues.getString(0));
+        assertEquals("VALUE_B", enumValues.getString(1));
+    }
+
+    public enum DummyEnum {
+        VALUE_A,
+        VALUE_B
+    }
+
+    @Extension
+    @Symbol("dummyConfig")
+    public static class DummyConfig extends GlobalConfiguration {
+        private List<DummyEnum> myEnums;
+
+        @DataBoundConstructor
+        public DummyConfig() {}
+
+        public List<DummyEnum> getMyEnums() {
+            return myEnums;
+        }
+
+        @DataBoundSetter
+        public void setMyEnums(List<DummyEnum> myEnums) {
+            this.myEnums = myEnums;
+        }
     }
 
     @Test
